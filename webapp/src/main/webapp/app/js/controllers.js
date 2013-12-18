4e23758f88ca45115e1feb8d4bec7fa2295ca427
Deborah Shapiro <deborah.seider.shapiro@gmail.com>
2013-12-17 16:13:22 -0800

    Webapp libraries and structure. Webapp with REST call to get MapProject.

---
+++ webapp/src/main/webapp/app/js/controllers.js
@@ -0,0 +1,34 @@
+'use strict';
+
+<!--mapProjectNgApp.controller('MyCtrl2', [function($scope) {
+    $scope.mapProjects = [{
+        name: 'SNOMEDCT to ICD10CM',
+        id: '1',
+        refSetId: '123456789',
+        sourceTerminology: 'SNOMEDCT',
+        sourceVersion: '20130731',
+        destinationTerminology: 'ICD10CM',
+        destinationVersion: '2010',
+        leads: [{name: 'Brian'}, {name: 'Rory'}],
+        specialists: [{name: 'Deborah'}, {name: 'Patrick'}],
+        advices: [{}],
+        published: 0,
+        blockStructure: 0,
+        groupStructure: 0
+    },{
+        name: 'SNOMEDCT to ICD9CM',
+        id: '2',
+        refSetId: '345678912',
+        sourceTerminology: 'SNOMEDCT',
+        sourceVersion: '20130731',
+        destinationTerminology: 'ICD9CM',
+        destinationVersion: '2010',
+        leads: [{name: 'Brian'}],
+        specialists: [{name: 'Deborah'}, {name: 'Patrick'}],
+        advices: [{name: 'advice1'}, {name: 'advice2'}],
+        published: 0,
+        blockStructure: 0,
+        groupStructure: 0
+    }]
+}]);-->
+
