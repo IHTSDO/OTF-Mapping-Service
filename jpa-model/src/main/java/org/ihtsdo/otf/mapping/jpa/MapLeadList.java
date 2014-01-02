d4e0a9db276f5b52f16364d704c5c6306f000397
granvold <granvold@gmail.com>
2014-01-02 10:14:07 -0800

    Added count objects to [Object]List elements in model-jpa.
    Project detail in webapp now displays the number of sub-elements in accordion-headers.

--- jpa-model/src/main/java/org/ihtsdo/otf/mapping/jpa/MapLeadList.java
+++ jpa-model/src/main/java/org/ihtsdo/otf/mapping/jpa/MapLeadList.java
@@ -83,5 +83,14 @@ public class MapLeadList {
 	public List<MapLead> getMapLeads() {
 		return mapLeads;
 	}
+	
+	/**
+	 * Return the count as an xml element
+	 * @return the number of objects in the list
+	 */
+	@XmlElement(name = "count")
+	public int getCount() {
+		return mapLeads.size();
+	}
 
 }
