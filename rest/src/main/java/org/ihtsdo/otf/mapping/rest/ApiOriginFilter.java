6da33cc74e6a698e269d38cb9ee0b7157c7485eb
bcarlsenca <bcarlsen@westcoastinformatics.com>
2014-01-03 19:10:11 -0800

    Next attempt to support CORS.

--- rest/src/main/java/org/ihtsdo/otf/mapping/rest/ApiOriginFilter.java
+++ rest/src/main/java/org/ihtsdo/otf/mapping/rest/ApiOriginFilter.java
@@ -1,3 +1,6 @@
+/*
+ * 
+ */
 package org.ihtsdo.otf.mapping.rest;
 
 import java.io.IOException;
@@ -11,15 +14,21 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * API origin filter (CORS)
+ */
 public class ApiOriginFilter implements Filter {
 
 
+	/* (non-Javadoc)
+	 * @see javax.servlet.Filter#doFilter(javax.servlet.ServletRequest, javax.servlet.ServletResponse, javax.servlet.FilterChain)
+	 */
 	public void doFilter(ServletRequest request, ServletResponse response,
 		FilterChain chain) throws IOException, ServletException {
 		HttpServletResponse res = (HttpServletResponse) response;
 		res.addHeader("Access-Control-Allow-Origin", "*");
     res.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, PATCH, OPTIONS");
-    res.addHeader("Access-Control-Allow-Headers", "Content-Type, api_key, Authorization");
+    res.addHeader("Access-Control-Allow-Headers", "Content-Type,X-Requested-With,accept,Origin,Access-Control-Request-Method,Access-Control-Request-Headers, api_key, Authorization");
     chain.doFilter(request, response);		
 	}
 
