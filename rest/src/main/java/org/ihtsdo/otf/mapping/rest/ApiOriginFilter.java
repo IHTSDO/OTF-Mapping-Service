3f4dd3957c8152c6afc97336ab3fcd9c48a50ec0
bcarlsenca <bcarlsen@westcoastinformatics.com>
2014-01-03 18:59:58 -0800

    Final fixes for CORS support.

--- rest/src/main/java/org/ihtsdo/otf/mapping/rest/ApiOriginFilter.java
+++ rest/src/main/java/org/ihtsdo/otf/mapping/rest/ApiOriginFilter.java
@@ -18,8 +18,8 @@ public class ApiOriginFilter implements Filter {
 		FilterChain chain) throws IOException, ServletException {
 		HttpServletResponse res = (HttpServletResponse) response;
 		res.addHeader("Access-Control-Allow-Origin", "*");
-    res.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
-    res.addHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, PATCH, OPTIONS");
+    res.addHeader("Access-Control-Allow-Headers", "Content-Type, api_key, Authorization");
     chain.doFilter(request, response);		
 	}
 
