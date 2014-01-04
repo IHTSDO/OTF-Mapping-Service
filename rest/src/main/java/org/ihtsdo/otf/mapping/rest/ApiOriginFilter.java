7acc83df18ff732652b26b5fa7cb80fb35f84233
bcarlsenca <bcarlsen@westcoastinformatics.com>
2014-01-03 18:50:41 -0800

    Add new filter

---
+++ rest/src/main/java/org/ihtsdo/otf/mapping/rest/ApiOriginFilter.java
@@ -0,0 +1,37 @@
+package org.ihtsdo.otf.mapping.rest;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ApiOriginFilter implements Filter {
+
+
+	public void doFilter(ServletRequest request, ServletResponse response,
+		FilterChain chain) throws IOException, ServletException {
+		HttpServletResponse res = (HttpServletResponse) response;
+		res.addHeader("Access-Control-Allow-Origin", "*");
+    res.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+    res.addHeader("Access-Control-Allow-Headers", "Content-Type");
+    chain.doFilter(request, response);		
+	}
+
+	@Override
+	public void destroy() {
+		// do nothing		
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// do nothing
+	}
+
+
+}
