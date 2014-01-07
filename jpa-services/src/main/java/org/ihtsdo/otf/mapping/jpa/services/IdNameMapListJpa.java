08abc7412026084c0e9de532f962d155517dc262
Deborah Shapiro <deborah.seider.shapiro@gmail.com>
2014-01-07 09:23:20 -0800

    IdNameMap, IdNameMapList for MetadataService use. New MapRecordJpaTest.java.

---
+++ jpa-services/src/main/java/org/ihtsdo/otf/mapping/jpa/services/IdNameMapListJpa.java
@@ -0,0 +1,81 @@
+package org.ihtsdo.otf.mapping.jpa.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.ihtsdo.otf.mapping.services.IdNameMap;
+import org.ihtsdo.otf.mapping.services.IdNameMapList;
+
+/**
+ * Container for all of the metadata.
+ * Contains a list of 
+ * IdNameMaps that each contain one type of metadata.
+ *
+ * @author ${author}
+ */
+@XmlRootElement(name = "idNameMapList")
+public class IdNameMapListJpa implements IdNameMapList {
+
+
+	/** The id name map list. */
+	private List<IdNameMap> idNameMapList = new ArrayList<IdNameMap>();
+
+
+	/**
+	 * Instantiates an empty {@link IdNameMapListJpa}.
+	 */
+	public IdNameMapListJpa() {
+		// do nothing
+	}
+
+
+	/* (non-Javadoc)
+	 * @see org.ihtsdo.otf.mapping.services.IdNameMapList#getIdNameMapList()
+	 */
+	@Override
+	@XmlElement(type=IdNameMapJpa.class, name = "idNameMaps")
+	public List<IdNameMap> getIdNameMapList() {
+		return idNameMapList;
+	}
+
+  
+	/* (non-Javadoc)
+	 * @see org.ihtsdo.otf.mapping.services.IdNameMapList#setIdNameMapList(java.util.List)
+	 */
+	@Override
+	public void setIdNameMapList(List<IdNameMap> idNameMapList) {
+		this.idNameMapList = idNameMapList;
+	}
+	
+
+	/* (non-Javadoc)
+	 * @see org.ihtsdo.otf.mapping.services.IdNameMapList#getCount()
+	 */
+	@Override
+	@XmlElement(name = "count")
+	public int getCount() {
+		return idNameMapList.size();
+	}
+
+
+	/* (non-Javadoc)
+	 * @see org.ihtsdo.otf.mapping.services.IdNameMapList#addIdNameMap(org.ihtsdo.otf.mapping.services.IdNameMap)
+	 */
+	@Override
+	public void addIdNameMap(IdNameMap idNameMap) {
+		idNameMapList.add(idNameMap);
+		
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.ihtsdo.otf.mapping.services.IdNameMapList#removeIdNameMap(org.ihtsdo.otf.mapping.services.IdNameMap)
+	 */
+	@Override
+	public void removeIdNameMap(IdNameMap idNameMap) {
+		idNameMapList.remove(idNameMap);
+		
+	}
+}
--- services/src/main/java/org/ihtsdo/otf/mapping/services/ContentService.java
+++ services/src/main/java/org/ihtsdo/otf/mapping/services/ContentService.java
@@ -1,10 +1,14 @@
 package org.ihtsdo.otf.mapping.services;
 
+import java.util.Set;
+
 import org.ihtsdo.otf.mapping.rf2.Concept;
 
+// TODO: Auto-generated Javadoc
 /**
- * The interface for the content service
+ * The interface for the content service.
  *
+ * @author ${author}
  */
 public interface ContentService {
 	
@@ -34,4 +38,15 @@ public interface ContentService {
 	 * @throws Exception if anything goes wrong
 	 */
 	public SearchResultList findConcepts(String searchString) throws Exception;
+	
+	/**
+	 * Returns the descendants.
+	 *
+	 * @param concept the concept
+	 * @param terminology the terminology
+	 * @param terminologyVersion the terminology version
+	 * @param descendantResultSet the descendant result set
+	 * @return the descendants
+	 */
+	public void getDescendants(Concept concept, String terminology, String terminologyVersion, Set<Concept> descendantResultSet);
 }
