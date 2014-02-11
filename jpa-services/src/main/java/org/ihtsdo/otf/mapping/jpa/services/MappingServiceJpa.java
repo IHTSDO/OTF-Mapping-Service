987cfc1134cd9ac7c79ab2023925c2d62ceb90f1
granvold <pgranvold@westcoastinformatics.com>
2014-02-10 17:21:13 -0800

    Webapp and some service changes. - unmapped descendant alerts - icon legends - general formatting - general code cleanup

--- jpa-services/src/main/java/org/ihtsdo/otf/mapping/jpa/services/MappingServiceJpa.java
+++ jpa-services/src/main/java/org/ihtsdo/otf/mapping/jpa/services/MappingServiceJpa.java
@@ -1565,7 +1565,7 @@ public class MappingServiceJpa implements MappingService {
 	 */
 	public List<Concept> getUnmappedDescendantsForConcept(Concept concept,
 			int thresholdLlc) throws Exception {
-
+		
 		// declare results list and content service
 		List<Concept> unmappedDescendants = new ArrayList<Concept>();
 		ContentService contentService = new ContentServiceJpa();
@@ -1575,11 +1575,12 @@ public class MappingServiceJpa implements MappingService {
 				concept.getTerminologyId(), concept.getTerminology(),
 				concept.getTerminologyVersion(), new Long("116680003"));
 		Iterator<Concept> descendants_iter = descendants.iterator();
-
+		
+	
 		// if size of descendant set is greater than the low-level concept
 		// threshold, skip it
 		if (descendants.size() <= thresholdLlc) {
-
+		
 			// cycle over descendants
 			while (descendants_iter.hasNext()) {
 
@@ -1589,13 +1590,8 @@ public class MappingServiceJpa implements MappingService {
 				List<MapRecord> conceptRecords = getMapRecordsForConceptId(descendant
 						.getTerminologyId());
 
-				System.out.println(descendant.getTerminologyId() + " has "
-						+ Integer.toString(conceptRecords.size())
-						+ " map records");
-
 				// if no records found, add to unmapped list
 				if (conceptRecords.size() == 0) {
-					System.out.println("--> Adding to unmapped list");
 					unmappedDescendants.add(descendant);
 				}
 			}
