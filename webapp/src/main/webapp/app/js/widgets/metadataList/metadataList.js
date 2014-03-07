4229c869e929ffb1edfe610eed6e2865fc5e147d
Deborah Shapiro <deborah.seider.shapiro@gmail.com>
2014-03-07 15:36:22 -0800

    MAP-59: metadata display fixes

--- webapp/src/main/webapp/app/js/widgets/metadataList/metadataList.js
+++ webapp/src/main/webapp/app/js/widgets/metadataList/metadataList.js
@@ -1,26 +1,3 @@
-/*
- * The MIT License
- * 
- * Copyright (c) 2013, Sebastian Sdorra
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
 
 'use strict';
 
@@ -34,11 +11,10 @@ angular.module('mapProjectApp.widgets.metadataList', ['adf.provider'])
         controller: 'metadataCtrl',
         resolve: {
           data: function(metadataService, config){
-            if (config.terminology){
-              return metadataService.get(config.terminology);
-            } else {
-              return metadataService.get('SNOMEDCT');
-            }
+            if (!config.terminology){
+                config.terminology = 'SNOMEDCT';
+            } 
+            return metadataService.get(config.terminology);
           }
         },
         edit: {
@@ -65,7 +41,6 @@ angular.module('mapProjectApp.widgets.metadataList', ['adf.provider'])
 	              }
 		  }).error(function() {
               deferred.reject();
-			  $scope.errorMetadata = "Error retrieving all metadata";
 		 });
         return deferred.promise;
       }
