9f8bf391cd323828ec17be49f2e128b35b3e6a1e
bcarlsenca <bcarlsen@westcoastinformatics.com>
2014-04-03 14:46:09 -0700

    MAP-169, MAP-106: fixes to header and put back commented out code.

--- jpa-model/src/main/java/org/ihtsdo/otf/mapping/jpa/MapRecordJpa.java
+++ jpa-model/src/main/java/org/ihtsdo/otf/mapping/jpa/MapRecordJpa.java
@@ -20,7 +20,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 import javax.xml.bind.annotation.XmlElement;
