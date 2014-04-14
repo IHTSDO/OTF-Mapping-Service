92b43065463236f12cf9af445c3cff8adece6e74
Deborah Shapiro <deborah.seider.shapiro@gmail.com>
2014-04-14 16:53:56 -0700

    MAP-31: conflict resolution compare records widget improvements

--- webapp/src/main/webapp/app/js/app.js
+++ webapp/src/main/webapp/app/js/app.js
@@ -133,7 +133,7 @@ mapProjectApp.config(['$routeProvider',
 		  controller: 'MapRecordDashboardCtrl'
 	  });
 	  
-	  $routeProvider.when('/record/conflicts/:conceptId', {
+	  $routeProvider.when('/record/conflicts/:recordId', {
 		  templateUrl: 'partials/record-dashboard.html',
 		  controller: 'ResolveConflictsDashboardCtrl'
 	  });
