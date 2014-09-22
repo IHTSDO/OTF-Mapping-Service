2e37d7299a3bab48bb73d6795d515aeeb0ee7b51
granvold <pgranvold@westcoastinformatics.com>
2014-09-22 10:28:46 -0700

    MAP-831:  Added mechanism to compute workflow to track added/removed concepts via log MAP-832:  Fixed error in unassigning/canceling FIX_ERROR_PATH concepts

--- admin/mojo/src/main/java/org/ihtsdo/otf/mapping/mojo/ComputeWorkflowLoaderMojo.java
+++ admin/mojo/src/main/java/org/ihtsdo/otf/mapping/mojo/ComputeWorkflowLoaderMojo.java
@@ -1,6 +1,8 @@
 package org.ihtsdo.otf.mapping.mojo;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import org.apache.maven.plugin.AbstractMojo;
@@ -10,6 +12,7 @@ import org.ihtsdo.otf.mapping.jpa.services.WorkflowServiceJpa;
 import org.ihtsdo.otf.mapping.model.MapProject;
 import org.ihtsdo.otf.mapping.services.MappingService;
 import org.ihtsdo.otf.mapping.services.WorkflowService;
+import org.ihtsdo.otf.mapping.workflow.TrackingRecord;
 
 /**
  * Loads unpublished complex maps.
@@ -40,7 +43,7 @@ import org.ihtsdo.otf.mapping.services.WorkflowService;
  *           </plugin>
  *         </plugins>
  *       </build>
- *     </profile> 
+ *     </profile>
  * </pre>
  * 
  * @goal compute-workflow
@@ -48,56 +51,99 @@ import org.ihtsdo.otf.mapping.services.WorkflowService;
  */
 public class ComputeWorkflowLoaderMojo extends AbstractMojo {
 
-  /**
-   * The refSet id
-   * @parameter refSetId
-   */
-  private String refSetId = null;
+	/**
+	 * The refSet id
+	 * 
+	 * @parameter refSetId
+	 */
+	private String refSetId = null;
 
-  /**
-   * Executes the plugin.
-   * 
-   * @throws MojoExecutionException the mojo execution exception
-   */
-  @Override
-  public void execute() throws MojoExecutionException {
-    getLog().info("Starting compute workflow - " + refSetId);
+	/**
+	 * Executes the plugin.
+	 * 
+	 * @throws MojoExecutionException
+	 *             the mojo execution exception
+	 */
+	@Override
+	public void execute() throws MojoExecutionException {
+		getLog().info("Starting compute workflow - " + refSetId);
 
-    if (refSetId == null) {
-      throw new MojoExecutionException("You must specify a refSetId.");
-    }
+		if (refSetId == null) {
+			throw new MojoExecutionException("You must specify a refSetId.");
+		}
 
-    try {
+		try {
 
-      MappingService mappingService = new MappingServiceJpa();
-      Set<MapProject> mapProjects = new HashSet<>();
+			MappingService mappingService = new MappingServiceJpa();
+			Set<MapProject> mapProjects = new HashSet<>();
 
-      for (MapProject mapProject : mappingService.getMapProjects()
-          .getIterable()) {
-        for (String id : refSetId.split(",")) {
-          if (mapProject.getRefSetId().equals(id)) {
-            mapProjects.add(mapProject);
-          }
-        }
-      }
+			for (MapProject mapProject : mappingService.getMapProjects()
+					.getIterable()) {
+				for (String id : refSetId.split(",")) {
+					if (mapProject.getRefSetId().equals(id)) {
+						mapProjects.add(mapProject);
+					}
+				}
+			}
 
-      // Compute workflow
-      WorkflowService workflowService = new WorkflowServiceJpa();
-      for (MapProject mapProject : mapProjects) {
-        getLog().info(
-            "Computing workflow for " + mapProject.getName() + ", "
-                + mapProject.getId());
-        workflowService.computeWorkflow(mapProject);
-      }
+			// Get the current workflow and extract concepts for comparison
 
-      getLog().info("done ...");
-      mappingService.close();
-      workflowService.close();
+			WorkflowService workflowService = new WorkflowServiceJpa();
 
-    } catch (Exception e) {
-      e.printStackTrace();
-      throw new MojoExecutionException("Computing workflow failed.", e);
-    }
+			// Compute workflow
 
-  }
+			for (MapProject mapProject : mapProjects) {
+
+				Map<String, String> previousWorkflowConcepts = new HashMap<>();
+				int conceptsAdded = 0;
+				int conceptsRemoved = 0;
+
+				// add all current concepts with a tracking record to set
+				for (TrackingRecord tr : workflowService
+						.getTrackingRecordsForMapProject(mapProject)
+						.getIterable()) {
+					previousWorkflowConcepts.put(tr.getTerminologyId(), tr.getDefaultPreferredName());
+				}
+
+				getLog().info(
+						"Computing workflow for " + mapProject.getName() + ", "
+								+ mapProject.getId());
+				workflowService.computeWorkflow(mapProject);
+				
+				getLog().info(
+						"Comparing new workflow to previous workflow for " + mapProject.getName() + ", "
+								+ mapProject.getId());
+				
+				// cycle over new workflow
+				for (TrackingRecord tr : workflowService
+						.getTrackingRecordsForMapProject(mapProject)
+						.getIterable()) {
+					
+					if (!previousWorkflowConcepts.containsKey(tr.getTerminologyId())) {
+						getLog().info("  New concept:  " + tr.getTerminologyId() + ", " + tr.getDefaultPreferredName());
+						previousWorkflowConcepts.remove(tr.getTerminologyId());
+						conceptsAdded++;
+					}
+				}
+				
+				// cycle over remaining concepts in old workflow, which were removed by computing new workflow
+				for (String terminologyId : previousWorkflowConcepts.keySet()) {
+					getLog().info("  Removed concept:  " + terminologyId + ", " + previousWorkflowConcepts.get(terminologyId));
+					conceptsRemoved++;
+				}
+				
+				getLog().info("Workflow summary: " + conceptsAdded + " concepts added, " + conceptsRemoved + " concepts removed");
+
+			}
+
+			getLog().info("done ...");
+			mappingService.close();
+			workflowService.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new MojoExecutionException("Computing workflow failed.", e);
+		}
+
+	}
 }
