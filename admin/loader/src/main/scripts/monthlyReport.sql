38e7c260201619e52435190f67bf68f0e97b37d3
Deborah Shapiro <deborah.seider.shapiro@gmail.com>
2014-09-24 16:44:13 -0700

    MAP-775 Rework of manual sql reporting capabilities to include formatting and "by project" for every query.

--- admin/loader/src/main/scripts/monthlyReport.sql
+++ admin/loader/src/main/scripts/monthlyReport.sql
@@ -1,8 +1,10 @@
+-- add headers
+select 'Name' name, 'Project', 'Value', 'Date', 'Count';
 
 -- Monthly KPI status for reporting
 
 -- Total mapped by project 
-select 'Project productivity' name, mp.name, mr.workflowStatus value, DATE_FORMAT(from_unixtime(mr.lastModified/1000),'%Y-%m') date, count(*) 
+select 'Project productivity' name, mp.name, mr.workflowStatus value, DATE_FORMAT(from_unixtime(mr.lastModified/1000),'%Y-%m') date, count(*) ct
 from map_records mr, map_projects mp 
 where mr.mapProjectId = mp.id
 and from_unixtime(lastModified/1000) > date('2014-06-01')
@@ -12,39 +14,46 @@ group by mp.name, mr.workflowStatus,
    DATE_FORMAT(from_unixtime(mr.lastModified/1000),'%Y-%m');
 
 -- Number of errors by specialist
-select 'Specialist errors' name, userName, '' value, DATE_FORMAT(timestamp,'%Y-%m') date, count(*) 
-from feedbacks, map_users, feedback_recipients 
-where isError = '1' and feedback_recipients.feedbacks_id = feedbacks.id 
+select 'Specialist errors' name, map_projects.name, userName value, DATE_FORMAT(timestamp,'%Y-%m') date, count(*) ct 
+from feedbacks, map_users, feedback_recipients, map_projects, feedback_conversations
+where isError = '1' 
+and feedbacks.feedbackConversation_id = feedback_conversations.id
+and map_projects.id = feedback_conversations.mapProjectId
+and feedback_recipients.feedbacks_id = feedbacks.id 
 and feedback_recipients.recipients_id = map_users.id 
 and timestamp > date('2014-06-01')
 and timestamp < date('2014-12-01')
-group by recipients_id,
+group by map_projects.name, recipients_id,
    DATE_FORMAT(timestamp,'%Y-%m');
 
 -- Total number of errors by type and specialist 
-select 'Specialist errors by type' name, userName, mapError value, DATE_FORMAT(timestamp,'%Y-%m') date, count(*) 
-from feedbacks, map_users, feedback_recipients where isError = '1' 
+select 'Specialist errors by type' name, map_projects.name name, concat(userName, ' ', mapError) value, DATE_FORMAT(timestamp,'%Y-%m') date, count(*) ct
+from feedbacks, map_users, feedback_recipients , map_projects, feedback_conversations
+where isError = '1' 
+and feedbacks.feedbackConversation_id = feedback_conversations.id
+and map_projects.id = feedback_conversations.mapProjectId
 and timestamp > date('2014-06-01')
 and timestamp < date('2014-12-01')
 and feedback_recipients.feedbacks_id = feedbacks.id  
 and feedback_recipients.recipients_id = map_users.id 
-group by recipients_id, mapError,
+group by map_projects.name, recipients_id, mapError,
    DATE_FORMAT(timestamp,'%Y-%m')
 ORDER BY 2,3,4;
 
 -- Principles being applied 
-SELECT 'Map Principles' name, map_principles.id name, name value, DATE_FORMAT(from_unixtime(timestamp/1000),'%Y-%m') date, count(*) 
-from map_principles, map_records_map_principles, map_records 
+SELECT 'Map Principles' name, map_projects.name name, concat(map_principles.principleId, ' ', map_principles.name) value, DATE_FORMAT(from_unixtime(timestamp/1000),'%Y-%m') date, count(*) ct
+from map_principles, map_records_map_principles, map_records, map_projects
 where mapPrinciples_id = map_principles.id 
 and map_records_id = map_records.id 
+and map_records.mapProjectId = map_projects.id
 and from_unixtime(timestamp/1000) > date('2014-06-01')
 and from_unixtime(timestamp/1000) < date('2014-12-01')
-group by name,
+group by map_projects.name, map_principles.principleId, 
    DATE_FORMAT(from_unixtime(timestamp/1000),'%Y-%m')
 ORDER BY 2,3,4;
 
 -- Flagged for Map Lead Review by project
-select 'Flagged for map lead review' name, map_projects.name name, '' value, DATE_FORMAT(from_unixtime(timestamp/1000),'%Y-%m') date, count(*)
+select 'Flagged for map lead review' name, map_projects.name name, '' value, DATE_FORMAT(from_unixtime(timestamp/1000),'%Y-%m') date, count(*) ct
 from map_records, map_projects 
 where flagForMapLeadReview is true
 and map_records.mapProjectId = map_projects.id
@@ -55,7 +64,7 @@ group by mapProjectid,
 ORDER BY 2,3,4;
 
 -- Flagged for Discrepancy Review by project
-select 'Flagged for discrepancy review' name, map_projects.name name, '' value, DATE_FORMAT(lastModified,'%Y-%m') date, count(*)
+select 'Flagged for discrepancy review' name, map_projects.name name, '' value, DATE_FORMAT(lastModified,'%Y-%m') date, count(*) ct
 from feedback_conversations, map_projects 
 where isDiscrepancyReview is true 
 and feedback_conversations.mapProjectId = map_projects.id
@@ -67,15 +76,16 @@ ORDER BY 2,3,4;
 
 
 -- Total mapped by map project (#concepts Published + #concepts in Ready for Publication)
-select 'Total mapped' name, map_projects.name name, '' value, '' date, count(distinct conceptId)
+select 'Total mapped' name, map_projects.name name,
+   '' value, '' date, count(distinct conceptId)
 from map_records, map_projects
 where map_records.mapProjectId = map_projects.id
-and workflowStatus = 'PUBLISHED' || workflowStatus = 'READY_FOR_PUBLICATION'
+and workflowStatus in ('PUBLISHED','READY_FOR_PUBLICATION')
 group by mapProjectid
 ORDER BY 2,3,4;
 
 -- Total waiting for leads to claim for confict resolution
-select 'Available for conflict resolution' name, mp.name, '' value, '' date, count(*)
+select 'Available for conflict resolution' name, mp.name, '' value, '' date, count(*) ct
 from tracking_records, map_projects mp
 where tracking_records.mapProjectId = mp.id
 and  userAndWorkflowStatusPairs LIKE '%CONFLICT_DETECTED%'
@@ -84,29 +94,14 @@ and  userAndWorkflowStatusPairs NOT LIKE '%CONFLICT_IN_PROGRESS%'
 and  userAndWorkflowStatusPairs NOT LIKE '%CONFLICT_NEW%'
 group by mp.name;
 
--- Total concepts outstanding to be checked out
--- need help on logic (need to make distinct?)
-select 'Available for checkout' name, mp.name, '' value, '' date, count(*)
-from tracking_records, map_projects mp, map_records mr
-where tracking_records.mapProjectId = mp.id
-and mr.mapProjectId = mp.id
-and workflowStatus not in ('READY_FOR_PUBLICATION', 'PUBLISHED')
-and  mr.conceptId not in (select terminologyId from tracking_records)
-group by mp.name;
-
- select distinct(conceptId) from tracking_records, map_records where workflowStatus not in ('READY_FOR_PUBLICATION', 'PUBLISHED') and conceptId not in (select terminologyId from tracking_records);
-
 -- Total concepts that require finishing by specialists
-select 'Require finishing by specialist' name, mp.name, '' value, '' date, count(*)
+-- NOTE: this assumes NON_LEGACY_PATH or FIX_ERROR_PATH
+select 'Requires specialist to finish editing' name, mp.name, '' value, '' date, count(*) ct
 from tracking_records, map_projects mp
 where tracking_records.mapProjectId = mp.id
-and  (
-(assignedUserCount = 1 and userAndWorkflowStatusPairs LIKE '%EDITING_IN_PROGRESS%')
-or
-(assignedUserCount = 1 and userAndWorkflowStatusPairs LIKE '%EDITING_DONE%')
-or
-(assignedUserCount = 2 and userAndWorkflowStatusPairs LIKE '%EDITING_IN_PROGRESS%')
-)
+and assignedUserCount > 0
+and userAndWorkflowStatusPairs not like '%CONFLICT_DETECTED%'
+and userAndWorkflowStatusPairs not like '%REVIEW_NEEDED%'
 group by mp.name;
 
 
@@ -130,25 +125,47 @@ group by mp.name, mu.userName,
 ORDER BY 2,3,4;
 
 
--- Total concepts in this release (#concepts in Ready for Publication + # concepts in tracking_records) (make by map project)
+-- Total concepts in this release (by map project)
+--  (#concepts finished + #concepts in workflow)
+--  (#concepts in Ready for Publication + # concepts in tracking_records) 
 -- need help on syntax
-select  ( 
-select count(distinct conceptId) 
-from map_records 
-where workflowStatus = 'READY_FOR_PUBLICATION'
-) + ( 
-select count(distinct terminologyId) 
-from tracking_records) 
-as count;
 
-select 'Total concepts' name ( 
-select count(distinct conceptId) 
-from map_records , map_projects
-where workflowStatus = 'READY_FOR_PUBLICATION'
-and map_projects.id = map_records.mapProjectId
-) + ( 
-select count(distinct terminologyId) 
-from tracking_records) 
-as count
-group by mapProjectId;
+select 'Total concepts by workflow status' name,
+   mp.name, workflowStatus value, '' date, count(*) ct
+from map_projects mp,
+(select mapProjectId, conceptId, 'Ready for publication' workflowStatus
+ from map_records where workflowStatus = 'READY_FOR_PUBLICATION'
+ union
+ select mapProjectId, conceptId, 'Published'
+ from map_records where workflowStatus = 'PUBLISHED'
+ union
+ select mapProjectId, terminologyId, 'In progress'
+ from tracking_records) data
+where mp.id = data.mapProjectId
+group by mp.id, workflowStatus;
+
+select 'Total concepts edited' name,
+   mp.name, '' value, '' date, count(*) ct
+from map_projects mp,
+(select mapProjectId, conceptId
+ from map_records where workflowStatus = 'READY_FOR_PUBLICATION'
+ union
+ select mapProjectId, terminologyId
+ from tracking_records) data
+where mp.id = data.mapProjectId
+group by mp.id;
+
+
+-- Total concepts outstanding to be mapped
+-- This is essentially the same as the available work count
+-- look for tracking records where assignedUserCount = 0
+select 'Total concepts outstanding to be mapped' name,
+    mp.name project, '' value, '' date, ifnull(ct,0) ct
+from map_projects mp
+left outer join 
+(select count(*) ct, mapProjectId
+ from tracking_records 
+ where assignedUserCount = 0
+ group by mapProjectId) tr
+on tr.mapProjectId = mp.id;
 
