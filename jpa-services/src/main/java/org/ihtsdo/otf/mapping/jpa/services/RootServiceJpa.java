a647e9bb15e934928988f5d3bb668cef91e95d8d
bcarlsenca <bcarlsen@westcoastinformatics.com>
2014-10-06 08:11:57 -0700

    Fix warnings and move content service specific logic to content service.

--- jpa-services/src/main/java/org/ihtsdo/otf/mapping/jpa/services/RootServiceJpa.java
+++ jpa-services/src/main/java/org/ihtsdo/otf/mapping/jpa/services/RootServiceJpa.java
@@ -25,208 +25,183 @@ import org.ihtsdo.otf.mapping.services.RootService;
  */
 public class RootServiceJpa implements RootService {
 
-	/** The factory. */
-	protected static EntityManagerFactory factory;
+  /** The factory. */
+  protected static EntityManagerFactory factory;
 
-	/** The indexed field names. */
-	protected static Set<String> fieldNames;
-	
-	protected static Set<String> treePositionFieldNames;
+  /** The indexed field names. */
+  protected static Set<String> fieldNames;
 
-	/** The lock. */
-	private static String lock = "lock";
+  /** The lock. */
+  private static String lock = "lock";
 
-	/** The manager. */
-	protected EntityManager manager;
+  /** The manager. */
+  protected EntityManager manager;
 
-	/** The transaction per operation. */
-	protected boolean transactionPerOperation = true;
+  /** The transaction per operation. */
+  protected boolean transactionPerOperation = true;
 
-	/** The transaction entity. */
-	protected EntityTransaction tx;
+  /** The transaction entity. */
+  protected EntityTransaction tx;
 
-	/**
-	 * Instantiates an empty {@link RootServiceJpa}.
-	 * 
-	 * @throws Exception
-	 */
-	public RootServiceJpa() throws Exception {
-		// created once or if the factory has closed
-		synchronized (lock) {
-			if (factory == null || !factory.isOpen()) {
-				openFactory();
-			}
-		}
-		
-		// created on each instantiation
-		manager = factory.createEntityManager();
-		tx = manager.getTransaction();
-	}
+  /**
+   * Instantiates an empty {@link RootServiceJpa}.
+   * 
+   * @throws Exception
+   */
+  public RootServiceJpa() throws Exception {
+    // created once or if the factory has closed
+    synchronized (lock) {
+      if (factory == null || !factory.isOpen()) {
+        openFactory();
+      }
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.ihtsdo.otf.mapping.services.RootService#openFactory()
-	 */
-	@Override
-	public void openFactory() throws Exception {
+    // created on each instantiation
+    manager = factory.createEntityManager();
+    tx = manager.getTransaction();
+  }
 
-		// if factory has not been instantiated or has been closed, open it
-		if (factory == null || !factory.isOpen()) {
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.ihtsdo.otf.mapping.services.RootService#openFactory()
+   */
+  @Override
+  public void openFactory() throws Exception {
 
-			Logger.getLogger(this.getClass()).info(
-					"Setting root service entity manager factory.");
-			String configFileName = System.getProperty("run.config");
-			Logger.getLogger(this.getClass()).info(
-					"  run.config = " + configFileName);
-			Properties config = new Properties();
-			FileReader in = new FileReader(new File(configFileName));
-			config.load(in);
-			in.close();
-			Logger.getLogger(this.getClass()).info("  properties = " + config);
-			factory = Persistence.createEntityManagerFactory(
-					"MappingServiceDS", config);
-		}
+    // if factory has not been instantiated or has been closed, open it
+    if (factory == null || !factory.isOpen()) {
 
-		// if the field names have not been set, initialize
-		if (fieldNames == null)
-			initializeFieldNames();
-	}
+      Logger.getLogger(this.getClass()).info(
+          "Setting root service entity manager factory.");
+      String configFileName = System.getProperty("run.config");
+      Logger.getLogger(this.getClass())
+          .info("  run.config = " + configFileName);
+      Properties config = new Properties();
+      FileReader in = new FileReader(new File(configFileName));
+      config.load(in);
+      in.close();
+      Logger.getLogger(this.getClass()).info("  properties = " + config);
+      factory =
+          Persistence.createEntityManagerFactory("MappingServiceDS", config);
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.ihtsdo.otf.mapping.services.RootService#closeFactory()
-	 */
-	@Override
-	public void closeFactory() throws Exception {
-		if (factory.isOpen()) {
-			factory.close();
-		}
-	}
+    // if the field names have not been set, initialize
+    if (fieldNames == null)
+      initializeFieldNames();
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.ihtsdo.otf.mapping.services.RootService#initializeFieldNames()
-	 */
-	@Override
-	public void initializeFieldNames() throws Exception {
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.ihtsdo.otf.mapping.services.RootService#closeFactory()
+   */
+  @Override
+  public void closeFactory() throws Exception {
+    if (factory.isOpen()) {
+      factory.close();
+    }
+  }
 
-		if (fieldNames == null) {
-			fieldNames = new HashSet<>();
-			EntityManager manager = factory.createEntityManager();
-			FullTextEntityManager fullTextEntityManager = org.hibernate.search.jpa.Search
-					.getFullTextEntityManager(manager);
-			IndexReaderAccessor indexReaderAccessor = fullTextEntityManager
-					.getSearchFactory().getIndexReaderAccessor();
-			Set<String> indexedClassNames = fullTextEntityManager
-					.getSearchFactory().getStatistics().getIndexedClassNames();
-			for (String indexClass : indexedClassNames) {
-				IndexReader indexReader = indexReaderAccessor.open(indexClass);
-				try {
-					for (FieldInfo info : ReaderUtil
-							.getMergedFieldInfos(indexReader)) {
-						fieldNames.add(info.name);
-					}
-				} finally {
-					indexReaderAccessor.close(indexReader);
-				}
-			}
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.ihtsdo.otf.mapping.services.RootService#initializeFieldNames()
+   */
+  @Override
+  public void initializeFieldNames() throws Exception {
 
-			fullTextEntityManager.close();
-		}
-		
-		if (treePositionFieldNames == null) {
-			treePositionFieldNames = new HashSet<>();
-			EntityManager manager = factory.createEntityManager();
-			FullTextEntityManager fullTextEntityManager = org.hibernate.search.jpa.Search
-					.getFullTextEntityManager(manager);
-			IndexReaderAccessor indexReaderAccessor = fullTextEntityManager
-					.getSearchFactory().getIndexReaderAccessor();
-			
-			IndexReader indexReader = indexReaderAccessor.open("org.ihtsdo.otf.mapping.rf2.jpa.TreePositionJpa");
-			try {
-				for (FieldInfo info : ReaderUtil
-						.getMergedFieldInfos(indexReader)) {
-					treePositionFieldNames.add(info.name);
-				}
-			} finally {
-				indexReaderAccessor.close(indexReader);
-			}
-			
+    if (fieldNames == null) {
+      fieldNames = new HashSet<>();
+      EntityManager manager = factory.createEntityManager();
+      FullTextEntityManager fullTextEntityManager =
+          org.hibernate.search.jpa.Search.getFullTextEntityManager(manager);
+      IndexReaderAccessor indexReaderAccessor =
+          fullTextEntityManager.getSearchFactory().getIndexReaderAccessor();
+      Set<String> indexedClassNames =
+          fullTextEntityManager.getSearchFactory().getStatistics()
+              .getIndexedClassNames();
+      for (String indexClass : indexedClassNames) {
+        IndexReader indexReader = indexReaderAccessor.open(indexClass);
+        try {
+          for (FieldInfo info : ReaderUtil.getMergedFieldInfos(indexReader)) {
+            fieldNames.add(info.name);
+          }
+        } finally {
+          indexReaderAccessor.close(indexReader);
+        }
+      }
 
-			fullTextEntityManager.close();
-		}
-	}
+      fullTextEntityManager.close();
+    }
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.ihtsdo.otf.mapping.services.MappingService#getTransactionPerOperation
-	 * ()
-	 */
-	@Override
-	public boolean getTransactionPerOperation() {
-		return transactionPerOperation;
-	}
+  /*
+   * (non-Javadoc)
+   * 
+   * @see
+   * org.ihtsdo.otf.mapping.services.MappingService#getTransactionPerOperation
+   * ()
+   */
+  @Override
+  public boolean getTransactionPerOperation() {
+    return transactionPerOperation;
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.ihtsdo.otf.mapping.services.MappingService#setTransactionPerOperation
-	 * (boolean)
-	 */
-	@Override
-	public void setTransactionPerOperation(boolean transactionPerOperation) {
-		this.transactionPerOperation = transactionPerOperation;
-	}
+  /*
+   * (non-Javadoc)
+   * 
+   * @see
+   * org.ihtsdo.otf.mapping.services.MappingService#setTransactionPerOperation
+   * (boolean)
+   */
+  @Override
+  public void setTransactionPerOperation(boolean transactionPerOperation) {
+    this.transactionPerOperation = transactionPerOperation;
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.ihtsdo.otf.mapping.services.MappingService#beginTransaction()
-	 */
-	@Override
-	public void beginTransaction() {
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.ihtsdo.otf.mapping.services.MappingService#beginTransaction()
+   */
+  @Override
+  public void beginTransaction() {
 
-		if (getTransactionPerOperation())
-			throw new IllegalStateException(
-					"Error attempting to begin a transaction when using transactions per operation mode.");
-		else if (tx != null && tx.isActive())
-			throw new IllegalStateException(
-					"Error attempting to begin a transaction when there "
-							+ "is already an active transaction");
-		tx = manager.getTransaction();
-		tx.begin();
-	}
+    if (getTransactionPerOperation())
+      throw new IllegalStateException(
+          "Error attempting to begin a transaction when using transactions per operation mode.");
+    else if (tx != null && tx.isActive())
+      throw new IllegalStateException(
+          "Error attempting to begin a transaction when there "
+              + "is already an active transaction");
+    tx = manager.getTransaction();
+    tx.begin();
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.ihtsdo.otf.mapping.services.MappingService#commit()
-	 */
-	@Override
-	public void commit() {
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.ihtsdo.otf.mapping.services.MappingService#commit()
+   */
+  @Override
+  public void commit() {
 
-		if (getTransactionPerOperation())
-			throw new IllegalStateException(
-					"Error attempting to commit a transaction when using transactions per operation mode.");
-		else if (tx != null && !tx.isActive())
-			throw new IllegalStateException(
-					"Error attempting to commit a transaction when there "
-							+ "is no active transaction");
-		tx.commit();
-	}
-	
-	@Override
-	public void close() throws Exception {
-		if (manager.isOpen()) {
-			manager.close();
-		}
-	}
+    if (getTransactionPerOperation())
+      throw new IllegalStateException(
+          "Error attempting to commit a transaction when using transactions per operation mode.");
+    else if (tx != null && !tx.isActive())
+      throw new IllegalStateException(
+          "Error attempting to commit a transaction when there "
+              + "is no active transaction");
+    tx.commit();
+  }
 
+  @Override
+  public void close() throws Exception {
+    if (manager.isOpen()) {
+      manager.close();
+    }
+  }
 
 }
