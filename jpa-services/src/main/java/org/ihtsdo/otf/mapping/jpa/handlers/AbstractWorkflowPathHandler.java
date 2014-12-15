5690c89c9eadd2744d5f2fed6d2b4ab34d9c02c3
granvold <pgranvold@westcoastinformatics.com>
2014-12-15 13:32:40 -0800

    MAP-961:  More fixes to release processing MAP-1015: Release stats file MAP-909:  Some files for workflow precondition checking

--- jpa-services/src/main/java/org/ihtsdo/otf/mapping/jpa/handlers/AbstractWorkflowPathHandler.java
+++ jpa-services/src/main/java/org/ihtsdo/otf/mapping/jpa/handlers/AbstractWorkflowPathHandler.java
@@ -1,98 +1,134 @@
 package org.ihtsdo.otf.mapping.jpa.handlers;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
+import org.ihtsdo.otf.mapping.helpers.MapRecordList;
+import org.ihtsdo.otf.mapping.helpers.MapRecordListJpa;
 import org.ihtsdo.otf.mapping.helpers.ValidationResult;
 import org.ihtsdo.otf.mapping.helpers.ValidationResultJpa;
+import org.ihtsdo.otf.mapping.helpers.WorkflowAction;
 import org.ihtsdo.otf.mapping.helpers.WorkflowStatus;
 import org.ihtsdo.otf.mapping.helpers.WorkflowStatusCombination;
 import org.ihtsdo.otf.mapping.jpa.services.MappingServiceJpa;
-import org.ihtsdo.otf.mapping.model.MapRecord;
+import org.ihtsdo.otf.mapping.model.MapUser;
 import org.ihtsdo.otf.mapping.services.MappingService;
 import org.ihtsdo.otf.mapping.services.helpers.WorkflowPathHandler;
 import org.ihtsdo.otf.mapping.workflow.TrackingRecord;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class AbstractWorkflowPathHandler.
+ * 
+ * @author ${author}
+ */
 public abstract class AbstractWorkflowPathHandler implements
     WorkflowPathHandler {
 
-  Set<WorkflowStatusCombination> allowableWorkflowStatusCombinations =
-      new HashSet<>();
+  /** The workflow combinations. */
+  private Set<WorkflowStatusCombination> workflowCombinations = new HashSet<>();
 
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.ihtsdo.otf.mapping.services.helpers.WorkflowPathHandler#
+   * validateTrackingRecord(org.ihtsdo.otf.mapping.workflow.TrackingRecord)
+   */
   @Override
   public ValidationResult validateTrackingRecord(TrackingRecord trackingRecord)
     throws Exception {
 
     ValidationResult result = new ValidationResultJpa();
 
-    // extract information from tracking record
-    Map<String, WorkflowStatus> usersToWorkflowStatusMap = new HashMap<>();
-
-    String userAndWorkflowStatusPairs =
-        trackingRecord.getUserAndWorkflowStatusPairs();
-    for (String userAndWorkflowStatusPair : userAndWorkflowStatusPairs
-        .split(" ")) {
-      int index = userAndWorkflowStatusPair.lastIndexOf("_");
-      WorkflowStatus workflowStatus =
-          WorkflowStatus.valueOf(userAndWorkflowStatusPair.substring(0,
-              index - 1));
-      String userName = userAndWorkflowStatusPair.substring(index + 1);
-      usersToWorkflowStatusMap.put(userName, workflowStatus);
-    }
-
-    // CHECK: Combination of records is valid
-    WorkflowStatusCombination workflowStatusCombination =
-        new WorkflowStatusCombination(
-            (Set<WorkflowStatus>) usersToWorkflowStatusMap.values());
-    if (!this.allowableWorkflowStatusCombinations
-        .contains(workflowStatusCombination)) {
+    // check the workflow combination
+    if (!workflowCombinations.contains(this
+        .getWorkflowCombinationForTrackingRecord(trackingRecord))) {
       result
-          .addError("Tracking record has invalid workflow status combination");
+          .addError("Tracking record has invalid combination of reported workflow statuses for "
+              + trackingRecord.getWorkflowPath());
     }
 
-    // CHECK: Records exist and are correct for each user-workflow status pair
-    MappingService mappingService = new MappingServiceJpa();
-    for (Long id : trackingRecord.getMapRecordIds()) {
-      MapRecord mr = mappingService.getMapRecord(id);
-
-      if (mr == null) {
-        result
-            .addError("Tracking record references non-existent map record with id "
-                + id);
-      } else {
-
-        for (String userName : usersToWorkflowStatusMap.keySet()) {
-          if (!userName.equals(mr.getOwner().getUserName())) {
-            result
-                .addError("Tracking record has incorrect owner for map record with id "
-                    + mr.getId());
-          } else {
-            
-          }
-        }
-      }
-    }
-    mappingService.close();
+    // extract the user/workflow pairs
+    Set<String> userWorkflowPairs =
+        new HashSet<>(Arrays.asList(trackingRecord
+            .getUserAndWorkflowStatusPairs().split(" ")));
 
     return result;
   }
 
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.ihtsdo.otf.mapping.services.helpers.WorkflowPathHandler#
+   * validateTrackingRecordForActionAndUser
+   * (org.ihtsdo.otf.mapping.workflow.TrackingRecord,
+   * org.ihtsdo.otf.mapping.helpers.WorkflowAction,
+   * org.ihtsdo.otf.mapping.model.MapUser)
+   */
   @Override
   public ValidationResult validateTrackingRecordForActionAndUser(
-    TrackingRecord trackingRecord) {
-    // TODO Auto-generated method stub
+    TrackingRecord trackingRecord, WorkflowAction action, MapUser mapUser)
+    throws Exception {
+
+    // This must be overwritten by the individual handlers
     return null;
   }
 
-  public Set<WorkflowStatusCombination> getAllowableWorkflowStatusCombinations() {
-    return allowableWorkflowStatusCombinations;
+  /**
+   * Returns the workflow combinations.
+   * 
+   * @return the workflow combinations
+   */
+  public Set<WorkflowStatusCombination> getWorkflowCombinations() {
+    return workflowCombinations;
   }
 
-  public void setAllowableWorkflowStatusCombinations(
-    Set<WorkflowStatusCombination> allowableWorkflowStatusCombinations) {
-    this.allowableWorkflowStatusCombinations =
-        allowableWorkflowStatusCombinations;
+  /**
+   * Sets the workflow combinations.
+   * 
+   * @param workflowCombinations the workflow combinations
+   */
+  public void setWorkflowCombinations(
+    Set<WorkflowStatusCombination> workflowCombinations) {
+    this.workflowCombinations = workflowCombinations;
   }
+
+  /**
+   * Adds the workflow combination.
+   * 
+   * @param workflowCombination the workflow combination
+   */
+  public void addWorkflowCombination(
+    WorkflowStatusCombination workflowCombination) {
+    if (this.workflowCombinations == null)
+      workflowCombinations = new HashSet<>();
+    this.workflowCombinations.add(workflowCombination);
+  }
+
+  private WorkflowStatusCombination getWorkflowCombinationForTrackingRecord(
+    TrackingRecord tr) {
+    WorkflowStatusCombination workflowCombination =
+        new WorkflowStatusCombination();
+
+    for (String pair : tr.getUserAndWorkflowStatusPairs().split(" ")) {
+      workflowCombination.addWorkflowStatus(WorkflowStatus.valueOf(pair
+          .substring(0, pair.lastIndexOf("_"))));
+    }
+
+    return workflowCombination;
+  }
+
+  private MapRecordList getMapRecordsForTrackingRecord(TrackingRecord tr)
+    throws Exception {
+    MappingService mappingService = new MappingServiceJpa();
+    MapRecordList mapRecords = new MapRecordListJpa();
+    for (Long id : tr.getMapRecordIds()) {
+      mapRecords.addMapRecord(mappingService.getMapRecord(id));
+    }
+    mappingService.close();
+    return mapRecords;
+  }
+
 }
