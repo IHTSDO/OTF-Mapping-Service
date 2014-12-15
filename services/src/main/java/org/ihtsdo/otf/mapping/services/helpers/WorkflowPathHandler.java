ec57c07b535496c0b233b380584c3b9ae46be9c6
granvold <pgranvold@westcoastinformatics.com>
2014-12-15 13:32:40 -0800

    MAP-961:  More fixes to release processing MAP-1015: Release stats file MAP-909:  Some files for workflow precondition checking

--- services/src/main/java/org/ihtsdo/otf/mapping/services/helpers/WorkflowPathHandler.java
+++ services/src/main/java/org/ihtsdo/otf/mapping/services/helpers/WorkflowPathHandler.java
@@ -1,11 +1,19 @@
 package org.ihtsdo.otf.mapping.services.helpers;
 
+import java.util.Set;
+
 import org.ihtsdo.otf.mapping.helpers.ValidationResult;
+import org.ihtsdo.otf.mapping.helpers.WorkflowAction;
+import org.ihtsdo.otf.mapping.helpers.WorkflowStatusCombination;
+import org.ihtsdo.otf.mapping.model.MapUser;
 import org.ihtsdo.otf.mapping.workflow.TrackingRecord;
 
 public interface WorkflowPathHandler {
 
   public ValidationResult validateTrackingRecord(TrackingRecord trackingRecord) throws Exception;
   
-  public ValidationResult validateTrackingRecordForActionAndUser(TrackingRecord trackingRecord);
+  public ValidationResult validateTrackingRecordForActionAndUser(
+    TrackingRecord trackingRecord, WorkflowAction action, MapUser mapUser)
+    throws Exception;
+
 }
