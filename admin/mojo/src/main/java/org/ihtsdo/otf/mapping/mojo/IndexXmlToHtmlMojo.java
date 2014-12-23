20f8041255e73b5f659001c46bdd75d50fc2feef
granvold <pgranvold@westcoastinformatics.com>
2014-12-23 11:11:05 -0800

    MAP-909:  Workflow precondition checking Warning and import cleanups. Removed generate conflicts methods and calls (no longer used) from WorkflowService/Jpa/Rest

--- admin/mojo/src/main/java/org/ihtsdo/otf/mapping/mojo/IndexXmlToHtmlMojo.java
+++ admin/mojo/src/main/java/org/ihtsdo/otf/mapping/mojo/IndexXmlToHtmlMojo.java
@@ -82,7 +82,6 @@ public class IndexXmlToHtmlMojo extends AbstractMojo {
    * 
    * @throws MojoExecutionException the mojo execution exception
    */
-  @SuppressWarnings("resource")
   @Override
   public void execute() throws MojoExecutionException {
     getLog().info("Starting to convert index XML to HTML");
