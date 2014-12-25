ddc6753378d29b634cb932589be164bdd37d3d8d
bcarlsenca <bcarlsen@westcoastinformatics.com>
2014-12-24 17:11:07 -0800

    Fix date format from yyyymmdd to yyyyMMdd

--- admin/mojo/src/main/java/org/ihtsdo/otf/mapping/mojo/MapRecordRf2ComplexMapLoaderMojo.java
+++ admin/mojo/src/main/java/org/ihtsdo/otf/mapping/mojo/MapRecordRf2ComplexMapLoaderMojo.java
@@ -203,7 +203,7 @@ public class MapRecordRf2ComplexMapLoaderMojo extends AbstractMojo {
           new ArrayList<ComplexMapRefSetMember>());
     }
 
-    final SimpleDateFormat dt = new SimpleDateFormat("yyyymmdd");
+    final SimpleDateFormat dt = new SimpleDateFormat("yyyyMMdd");
     while ((line = complexMapReader.readLine()) != null) {
       line = line.replace("\r", "");
       String fields[] = line.split("\t");
