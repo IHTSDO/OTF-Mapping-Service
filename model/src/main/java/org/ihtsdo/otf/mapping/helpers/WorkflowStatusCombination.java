0de95a4a096d5c3f89bd17b97a475a4d3b6f0dd6
granvold <pgranvold@westcoastinformatics.com>
2014-12-29 13:53:14 -0800

    MAP-1041:  All unit tests and fixes for user/action workflow validation

--- model/src/main/java/org/ihtsdo/otf/mapping/helpers/WorkflowStatusCombination.java
+++ model/src/main/java/org/ihtsdo/otf/mapping/helpers/WorkflowStatusCombination.java
@@ -1,13 +1,14 @@
 package org.ihtsdo.otf.mapping.helpers;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 /**
  * The Class WorkflowStatusCombination.
- *
+ * 
  */
 public class WorkflowStatusCombination {
 
@@ -23,10 +24,11 @@ public class WorkflowStatusCombination {
   public WorkflowStatusCombination() {
 
   }
-  
+
   /**
-   * Instantiates a {@link WorkflowStatusCombination} from the specified parameters.
-   *
+   * Instantiates a {@link WorkflowStatusCombination} from the specified
+   * parameters.
+   * 
    * @param workflowStatuses the workflow statuses
    */
   public WorkflowStatusCombination(List<WorkflowStatus> workflowStatuses) {
@@ -35,32 +37,25 @@ public class WorkflowStatusCombination {
     }
   }
 
- 
-
   /**
    * Adds the workflow status.
-   *
+   * 
    * @param workflowStatus the workflow status
    */
   public void addWorkflowStatus(WorkflowStatus workflowStatus) {
 
-    // check for null map
-    if (workflowStatuses == null)
-      workflowStatuses = new HashMap<>();
-
     // if this contains this workflow status, increment count
-    workflowStatuses.put(
-        workflowStatus,
-        workflowStatuses.get(workflowStatus) == null ? 1 : workflowStatuses
-            .get(workflowStatus) + 1);
-
+    if (workflowStatuses.containsKey(workflowStatus)) {
+      workflowStatuses.put(workflowStatus,
+          workflowStatuses.get(workflowStatus) + 1);
+    } else {
+      workflowStatuses.put(workflowStatus, 1);
+    }
   }
-  
-  
 
   /**
    * Returns the workflow statuses.
-   *
+   * 
    * @return the workflow statuses
    */
   public Map<WorkflowStatus, Integer> getWorkflowStatuses() {
@@ -69,25 +64,25 @@ public class WorkflowStatusCombination {
 
   /**
    * Sets the workflow statuses.
-   *
+   * 
    * @param workflowStatuses the workflow statuses
    */
   public void setWorkflowStatuses(Map<WorkflowStatus, Integer> workflowStatuses) {
     this.workflowStatuses = workflowStatuses;
   }
 
-  
   /**
    * Indicates whether or not the workflow combination represents an empty state
-   *
+   * 
    * @return <code>true</code> if so, <code>false</code> otherwise
    */
   public boolean isEmpty() {
     return this.workflowStatuses.isEmpty();
   }
-  
+
   /**
-   * Function to return all workflow statuses as a list (duplicate values permitted)
+   * Function to return all workflow statuses as a list (duplicate values
+   * permitted)
    * @return
    */
   public List<WorkflowStatus> getWorkflowStatusesAsList() {
@@ -97,10 +92,13 @@ public class WorkflowStatusCombination {
         statuses.add(status);
       }
     }
+    Collections.sort(statuses);
     return statuses;
   }
 
-  /* (non-Javadoc)
+  /*
+   * (non-Javadoc)
+   * 
    * @see java.lang.Object#hashCode()
    */
   @Override
@@ -113,7 +111,9 @@ public class WorkflowStatusCombination {
     return result;
   }
 
-  /* (non-Javadoc)
+  /*
+   * (non-Javadoc)
+   * 
    * @see java.lang.Object#equals(java.lang.Object)
    */
   @Override
@@ -133,7 +133,9 @@ public class WorkflowStatusCombination {
     return true;
   }
 
-  /* (non-Javadoc)
+  /*
+   * (non-Javadoc)
+   * 
    * @see java.lang.Object#toString()
    */
   @Override
@@ -141,6 +143,5 @@ public class WorkflowStatusCombination {
     return "WorkflowStatusCombination [workflowStatuses=" + workflowStatuses
         + "]";
   }
-  
-  
+
 }
