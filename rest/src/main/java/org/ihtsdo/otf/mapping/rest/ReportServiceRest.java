a76782a748bb3f6a2d43d8ee508aa1c82aa6d9f9
granvold <pgranvold@westcoastinformatics.com>
2015-01-06 16:29:56 -0800

    MAP-1055:  Added confirmation dialog and field clear on successful add to Report Definitions Fixed weird String sorting issue -- maybe? Added CONFLICT_NEW to the case where a specialist re-edits a CONFLICT_NEW record, previously would have thrown error

--- rest/src/main/java/org/ihtsdo/otf/mapping/rest/ReportServiceRest.java
+++ rest/src/main/java/org/ihtsdo/otf/mapping/rest/ReportServiceRest.java
@@ -106,7 +106,7 @@ public class ReportServiceRest extends RootServiceRest {
       definitionList.sortBy(new Comparator<ReportDefinition>() {
         @Override
         public int compare(ReportDefinition o1, ReportDefinition o2) {
-          return o1.getName().compareTo(o2.getName());
+          return o1.getName().toLowerCase().compareTo(o2.getName().toLowerCase());
         }
       });
 
