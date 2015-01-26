ef3896c9ba1b8da3e23ff2484c5257e650b1b04c
bcarlsenca <bcarlsen@westcoastinformatics.com>
2015-01-26 10:56:02 -0800

    MAP-????: support removing reports in a date range - we can use this as an admin tool once a year to remove old reports no longer needed.

--- admin/mojo/src/main/java/org/ihtsdo/otf/mapping/mojo/ReportRemoverMojo.java
+++ admin/mojo/src/main/java/org/ihtsdo/otf/mapping/mojo/ReportRemoverMojo.java
@@ -1,5 +1,8 @@
 package org.ihtsdo.otf.mapping.mojo;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -24,9 +27,20 @@ public class ReportRemoverMojo extends AbstractMojo {
   /**
    * The refSet id
    * @parameter refsetId
-   * @required
    */
   private String refsetId = null;
+
+  /**
+   * An optional start date
+   * @parameter
+   */
+  private String startDate = null;
+
+  /**
+   * An optional end date
+   * @parameter
+   */
+  private String endDate = null;
 
   /**
    * Executes the plugin.
@@ -37,28 +51,51 @@ public class ReportRemoverMojo extends AbstractMojo {
   public void execute() throws MojoExecutionException {
     getLog().info("Starting to remove reports");
     getLog().info("  refsetId = " + refsetId);
+    getLog().info("  startDate = " + startDate);
+    getLog().info("  endDate = " + endDate);
 
     try {
+
+      // check params
+      if (refsetId == null && startDate == null && endDate == null) {
+        throw new Exception(
+            "This call will delete all reports, if you really want to do this, use a start date of 19700101");
+      }
+
+      // parse the dates
+      Date start = new Date(0);
+      Date end = new Date();
+      DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
+      if (startDate != null) {
+        start = dateFormat.parse(startDate);
+      }
+      if (endDate != null) {
+        end = dateFormat.parse(endDate);
+      }
 
       MappingService mappingService = new MappingServiceJpa();
       Set<MapProject> mapProjects = new HashSet<>();
 
-      for (MapProject mapProject : mappingService.getMapProjects()
-          .getIterable()) {
-        for (String id : refsetId.split(",")) {
-          if (mapProject.getRefSetId().equals(id)) {
-            mapProjects.add(mapProject);
+      if (refsetId == null) {
+        mapProjects = new HashSet<>(mappingService.getMapProjects().getMapProjects());
+      } else {
+        for (MapProject mapProject : mappingService.getMapProjects()
+            .getIterable()) {
+          for (String id : refsetId.split(",")) {
+            if (mapProject.getRefSetId().equals(id)) {
+              mapProjects.add(mapProject);
+            }
           }
         }
       }
-
-      // Clear workflow
+      
+      // Remove reports
       ReportService reportService = new ReportServiceJpa();
       for (MapProject mapProject : mapProjects) {
         getLog().info(
             "Removing reports for " + mapProject.getName() + ", "
                 + mapProject.getId());
-        reportService.removeReportsForMapProject(mapProject);
+        reportService.removeReportsForMapProject(mapProject, start, end);
       }
 
       mappingService.close();
