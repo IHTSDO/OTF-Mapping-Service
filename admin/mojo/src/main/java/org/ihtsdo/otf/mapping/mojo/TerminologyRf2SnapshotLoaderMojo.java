07a20622ae999cfce98611fda1fccc26b4b26fb1
granvold <pgranvold@westcoastinformatics.com>
2015-03-02 13:43:37 -0800

    notification logging updates

--- admin/mojo/src/main/java/org/ihtsdo/otf/mapping/mojo/TerminologyRf2SnapshotLoaderMojo.java
+++ admin/mojo/src/main/java/org/ihtsdo/otf/mapping/mojo/TerminologyRf2SnapshotLoaderMojo.java
@@ -16,6 +16,7 @@ import java.util.Properties;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoFailureException;
+import org.ihtsdo.otf.mapping.helpers.ValidationResult;
 import org.ihtsdo.otf.mapping.jpa.services.ContentServiceJpa;
 import org.ihtsdo.otf.mapping.jpa.services.MetadataServiceJpa;
 import org.ihtsdo.otf.mapping.rf2.AttributeValueRefSetMember;
@@ -38,6 +39,7 @@ import org.ihtsdo.otf.mapping.services.ContentService;
 import org.ihtsdo.otf.mapping.services.MetadataService;
 import org.ihtsdo.otf.mapping.services.helpers.ConfigUtility;
 import org.ihtsdo.otf.mapping.services.helpers.FileSorter;
+import org.ihtsdo.otf.mapping.services.helpers.OtfEmailHandler;
 
 import com.google.common.io.Files;
 
@@ -72,6 +74,12 @@ public class TerminologyRf2SnapshotLoaderMojo extends AbstractMojo {
    * @required
    */
   private String version;
+  
+  /**
+   * Whether to send email notification of any errors
+   * @parameter
+   */
+  private boolean sendNotification = false;
 
   /** the defaultPreferredNames type id. */
   private Long dpnTypeId = 900000000000003001L;
@@ -143,6 +151,7 @@ public class TerminologyRf2SnapshotLoaderMojo extends AbstractMojo {
     getLog().info("Starting loading RF2 data");
     getLog().info("  inputDir = " + inputDir);
     getLog().info("  terminology = " + terminology);
+    getLog().info("  sendNotification = " + sendNotification);
 
     try {
 
@@ -151,6 +160,23 @@ public class TerminologyRf2SnapshotLoaderMojo extends AbstractMojo {
 
       // Load config properties
       Properties config = ConfigUtility.getConfigProperties();
+      
+      // check that notifications can be sent if requested
+      String notificationRecipients =
+          config.getProperty("send.notification.recipients");
+      if (!sendNotification) {
+        getLog().info(
+            "No notifications will be sent as a result of workflow computation.");
+      }
+      if (sendNotification
+          && config.getProperty("send.notification.recipients") == null) {
+        throw new MojoFailureException(
+            "Email notification was requested, but no recipients were specified.");
+      } else {
+        getLog().info(
+            "Request to send notification email for any errors to recipients: "
+                + notificationRecipients);
+      }
 
       // Set the input directory
       File coreInputDir = new File(inputDir);
@@ -336,8 +362,18 @@ public class TerminologyRf2SnapshotLoaderMojo extends AbstractMojo {
           break;
         }
         getLog().info("    Compute tree from rootId " + conceptId);
-        contentService.computeTreePositions(terminology, version, isaRelType,
+        ValidationResult result = contentService.computeTreePositions(terminology, version, isaRelType,
             rootId);
+        if (sendNotification && !result.isValid()) {
+          OtfEmailHandler handler = new OtfEmailHandler();
+          handler
+              .sendValidationResultEmail(
+                  config.getProperty("notification.recipients"),
+                  "OTF-Mapping-Tool:  Errors in computing " + terminology + ", "
+                      + version + " hierarchical tree positions",
+                  "Hello,\n\nErrors were detected when computing hierarchical tree positions for "
+                      + terminology + ", " + version, result);
+        }
 
         // Close service
         contentService.close();
