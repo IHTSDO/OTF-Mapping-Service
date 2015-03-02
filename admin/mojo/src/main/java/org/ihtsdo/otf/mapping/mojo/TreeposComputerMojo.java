247cea502a4c7b835b7209a06cd51cf07d2863f9
granvold <pgranvold@westcoastinformatics.com>
2015-03-02 13:34:57 -0800

    Email notifications for tree positions failure

--- admin/mojo/src/main/java/org/ihtsdo/otf/mapping/mojo/TreeposComputerMojo.java
+++ admin/mojo/src/main/java/org/ihtsdo/otf/mapping/mojo/TreeposComputerMojo.java
@@ -1,12 +1,18 @@
 package org.ihtsdo.otf.mapping.mojo;
 
 import java.util.Map;
+import java.util.Properties;
 
 import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.ihtsdo.otf.mapping.helpers.ValidationResult;
+import org.ihtsdo.otf.mapping.helpers.ValidationResultJpa;
 import org.ihtsdo.otf.mapping.jpa.services.ContentServiceJpa;
 import org.ihtsdo.otf.mapping.jpa.services.MetadataServiceJpa;
 import org.ihtsdo.otf.mapping.services.ContentService;
+import org.ihtsdo.otf.mapping.services.helpers.ConfigUtility;
+import org.ihtsdo.otf.mapping.services.helpers.OtfEmailHandler;
 
 /**
  * Goal which loads an RF2 Snapshot of SNOMED CT data into a database.
@@ -41,6 +47,12 @@ public class TreeposComputerMojo extends AbstractMojo {
   private String rootIds;
 
   /**
+   * Whether to send email notifications of any errors (default: false)
+   * @parameter
+   */
+  private boolean sendNotification = false;
+
+  /**
    * Instantiates a {@link TreeposComputerMojo} from the specified parameters.
    * 
    */
@@ -60,6 +72,29 @@ public class TreeposComputerMojo extends AbstractMojo {
     getLog().info("  terminologyVersion = " + terminologyVersion);
     getLog().info("  rootIds = " + rootIds);
 
+    // check notification parameter requirements
+    Properties config;
+    try {
+      config = ConfigUtility.getConfigProperties();
+    } catch (Exception e1) {
+      throw new MojoFailureException("Could not retrieve parameters from conf file");
+    }
+    String notificationRecipients =
+        config.getProperty("send.notification.recipients");
+    if (!sendNotification) {
+      getLog().info(
+          "No notifications will be sent as a result of workflow computation.");
+    }
+    if (sendNotification
+        && config.getProperty("send.notification.recipients") == null) {
+      throw new MojoFailureException(
+          "Email notification was requested, but no recipients were specified.");
+    } else {
+      getLog().info(
+          "Request to send notification email for any errors to recipients: "
+              + notificationRecipients);
+    }
+
     try {
 
       // creating tree positions
@@ -76,12 +111,27 @@ public class TreeposComputerMojo extends AbstractMojo {
 
       // Walk up tree to the root
       // ASSUMPTION: single root
+      ValidationResult results = new ValidationResultJpa();
       for (String rootId : rootIds.split(",")) {
-        getLog().info("  Compute tree from rootId " + rootId + ", " + isaRelType);
-        contentService.computeTreePositions(terminology, terminologyVersion,
-            isaRelType, rootId);
+        getLog().info(
+            "  Compute tree from rootId " + rootId + ", " + isaRelType);
+        ValidationResult result =
+            contentService.computeTreePositions(terminology,
+                terminologyVersion, isaRelType, rootId);
+        results.merge(result);
       }
       contentService.close();
+
+      if (!results.isValid()) {
+        OtfEmailHandler handler = new OtfEmailHandler();
+        handler
+            .sendValidationResultEmail(
+                notificationRecipients,
+                "OTF-Mapping-Tool:  Errors in computing " + terminology + ", "
+                    + terminologyVersion + " hierarchical tree positions",
+                "Hello,\n\nErrors were detected when computing hierarchical tree positions for "
+                    + terminology + ", " + terminologyVersion, results);
+      }
 
       getLog().info("Done ...");
     } catch (Exception e) {
@@ -89,5 +139,4 @@ public class TreeposComputerMojo extends AbstractMojo {
       throw new MojoFailureException("Unexpected exception:", e);
     }
   }
-
 }
