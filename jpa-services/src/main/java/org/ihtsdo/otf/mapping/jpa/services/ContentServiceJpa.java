d0b7550ac5f1b0c2d40a1a925eead99296491618
granvold <pgranvold@westcoastinformatics.com>
2015-03-04 13:46:35 -0800

    Fixed conflict-resolution-introduced error

--- jpa-services/src/main/java/org/ihtsdo/otf/mapping/jpa/services/ContentServiceJpa.java
+++ jpa-services/src/main/java/org/ihtsdo/otf/mapping/jpa/services/ContentServiceJpa.java
@@ -1654,10 +1654,6 @@ public class ContentServiceJpa extends RootServiceJpa implements ContentService 
     // if concept is active
     if (concept.isActive()) {
 
-      if (ancestorPath.indexOf("~" + concept.getTerminologyId() + "~") != -1
-          || ancestorPath.endsWith("~" + concept.getTerminologyId())) {
-        throw new Exception("CYCLE DETECTED: " + ancestorPath + ", "
-            + concept.getTerminologyId());
       // extract the ancestor terminology ids
       Set<String> ancestors = new HashSet<>();
       for (String ancestor : ancestorPath.split("~"))
