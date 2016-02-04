d7b6d1064ac8e28e54a6bb245c146011d536255a
granvold <pgranvold@westcoastinformatics.com>
2016-02-04 12:04:24 -0800

    Fixed problem with auto-publishing of identical records in NON_LEGACY_PATH; Review Path work

--- services/src/main/java/org/ihtsdo/otf/mapping/services/helpers/WorkflowPathHandler.java
+++ services/src/main/java/org/ihtsdo/otf/mapping/services/helpers/WorkflowPathHandler.java
@@ -1,11 +1,14 @@
 package org.ihtsdo.otf.mapping.services.helpers;
 
+import java.util.Set;
+
 import org.ihtsdo.otf.mapping.helpers.MapUserRole;
 import org.ihtsdo.otf.mapping.helpers.PfsParameter;
 import org.ihtsdo.otf.mapping.helpers.SearchResultList;
 import org.ihtsdo.otf.mapping.helpers.ValidationResult;
 import org.ihtsdo.otf.mapping.helpers.WorkflowAction;
 import org.ihtsdo.otf.mapping.model.MapProject;
+import org.ihtsdo.otf.mapping.model.MapRecord;
 import org.ihtsdo.otf.mapping.model.MapUser;
 import org.ihtsdo.otf.mapping.services.WorkflowService;
 import org.ihtsdo.otf.mapping.workflow.TrackingRecord;
@@ -16,57 +19,84 @@ import org.ihtsdo.otf.mapping.workflow.TrackingRecord;
  */
 public interface WorkflowPathHandler {
 
-  /**
-   * Validate tracking record.
-   *
-   * @param trackingRecord the tracking record
-   * @return the validation result
-   * @throws Exception the exception
-   */
-  public ValidationResult validateTrackingRecord(TrackingRecord trackingRecord) throws Exception;
-  
-  /**
-   * Validate tracking record for action and user.
-   *
-   * @param trackingRecord the tracking record
-   * @param action the action
-   * @param mapUser the map user
-   * @return the validation result
-   * @throws Exception the exception
-   */
-  public ValidationResult validateTrackingRecordForActionAndUser(
-    TrackingRecord trackingRecord, WorkflowAction action, MapUser mapUser)
-    throws Exception;
-  
-  /**
-   * Find available work.
-   *
-   * @param mapProject the map project
-   * @param mapUser the map user
-   * @param query the query
-   * @param pfsParameter the pfs parameter
-   * @return the search result list
-   */
-  public SearchResultList findAvailableWork(MapProject mapProject, MapUser mapUser, MapUserRole userRole, String query, PfsParameter pfsParameter, WorkflowService workflowService ) throws Exception;
-  /**
-   * Find assigned work.
-   *
-   * @param mapProject the map project
-   * @param mapUser the map user
-   * @param query the query
-   * @param pfsParameter the pfs parameter
-   * @return the search result list
- * @throws Exception 
-   */
-  public SearchResultList findAssignedWork(MapProject mapProject, MapUser mapUser, MapUserRole userRole, String query, PfsParameter pfsParameter, WorkflowService workflowService) throws Exception;
-  
-  
-  
-  /**
-   * Gets the name.
-   *
-   * @return the name
-   */
-  public String getName();
+	/**
+	 * Validate tracking record.
+	 *
+	 * @param trackingRecord
+	 *            the tracking record
+	 * @return the validation result
+	 * @throws Exception
+	 *             the exception
+	 */
+	public ValidationResult validateTrackingRecord(TrackingRecord trackingRecord) throws Exception;
+
+	/**
+	 * Validate tracking record for action and user.
+	 *
+	 * @param trackingRecord
+	 *            the tracking record
+	 * @param action
+	 *            the action
+	 * @param mapUser
+	 *            the map user
+	 * @return the validation result
+	 * @throws Exception
+	 *             the exception
+	 */
+	public ValidationResult validateTrackingRecordForActionAndUser(TrackingRecord trackingRecord, WorkflowAction action,
+			MapUser mapUser) throws Exception;
+
+	/**
+	 * Find available work.
+	 *
+	 * @param mapProject            the map project
+	 * @param mapUser            the map user
+	 * @param userRole the user role
+	 * @param query            the query
+	 * @param pfsParameter            the pfs parameter
+	 * @param workflowService the workflow service
+	 * @return the search result list
+	 * @throws Exception the exception
+	 */
+	public SearchResultList findAvailableWork(MapProject mapProject, MapUser mapUser, MapUserRole userRole,
+			String query, PfsParameter pfsParameter, WorkflowService workflowService) throws Exception;
+
+	/**
+	 * Find assigned work.
+	 *
+	 * @param mapProject            the map project
+	 * @param mapUser            the map user
+	 * @param userRole the user role
+	 * @param query            the query
+	 * @param pfsParameter            the pfs parameter
+	 * @param workflowService the workflow service
+	 * @return the search result list
+	 * @throws Exception the exception
+	 */
+	public SearchResultList findAssignedWork(MapProject mapProject, MapUser mapUser, MapUserRole userRole, String query,
+			PfsParameter pfsParameter, WorkflowService workflowService) throws Exception;
+
+	/**
+	 * Gets the name.
+	 *
+	 * @return the name
+	 */
+	public String getName();
+
+	
+	/**
+	 * Process workflow action.
+	 *
+	 * @param trackingRecord the tracking record
+	 * @param workflowAction the workflow action
+	 * @param mapProject the map project
+	 * @param mapUser the map user
+	 * @param mapRecords the map records
+	 * @param mapRecord the map record
+	 * @return the sets the
+	 * @throws Exception the exception
+	 */
+	public Set<MapRecord> processWorkflowAction(TrackingRecord trackingRecord, WorkflowAction workflowAction,
+			MapProject mapProject, MapUser mapUser, Set<MapRecord> mapRecords, MapRecord mapRecord) throws Exception;
 
 }
