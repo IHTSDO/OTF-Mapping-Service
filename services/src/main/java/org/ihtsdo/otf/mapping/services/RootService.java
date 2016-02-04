6294303999438052c866444411d8174cf5f769e0
granvold <pgranvold@westcoastinformatics.com>
2016-02-04 11:42:33 -0800

    NON_LEGACY_PATH finished

--- services/src/main/java/org/ihtsdo/otf/mapping/services/RootService.java
+++ services/src/main/java/org/ihtsdo/otf/mapping/services/RootService.java
@@ -96,4 +96,7 @@ public interface RootService {
    */
   public <T> List<T> applyPfsToList(List<T> list, Class<T> clazz,
     int[] totalCt, PfsParameter pfs) throws Exception;
+
+  public <T> List<?> getQueryResults(String query, Class<?> fieldNamesKey, Class<?> clazz, PfsParameter pfs, int[] totalCt)
+		throws Exception;
 }
