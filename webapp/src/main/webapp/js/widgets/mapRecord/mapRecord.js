3aae1233f01b9fb5044c3fbfacade06cde50d346
bcarlsenca <bcarlsen@westcoastinformatics.com>
2016-02-09 09:53:30 -0800

    MAP-1078: Get identify working properly with new workflow, implement some extra rules.

--- webapp/src/main/webapp/js/widgets/mapRecord/mapRecord.js
+++ webapp/src/main/webapp/js/widgets/mapRecord/mapRecord.js
@@ -1466,17 +1466,8 @@ angular
         };
 
         $scope.getBrowserUrl = function() {
-          if ($scope.currentUser.userName === 'guest') {
-            return 'http://browser.ihtsdotools.org/index.html?perspective=full&conceptId1='
-              + $scope.conversation.terminologyId + '&edition=en-edition'
-              + '&server=https://browser-aws-1.ihtsdotools.org/api/snomed&langRefset=900000000000509007'
-              + '&acceptLicense=true';
-
-          } else {
-            return 'http://dailybuild.ihtsdotools.org/index.html?perspective=full&conceptId1='
-            + $scope.conversation.terminologyId + '&diagrammingMarkupEnabled=true&acceptLicense=true';
-          }
-          
+          return 'http://dailybuild.ihtsdotools.org/index.html?perspective=full&conceptId1='
+            + $scope.record.conceptId + '&diagrammingMarkupEnabled=true&acceptLicense=true';
         };
 
         $scope.openConceptBrowser = function() {
