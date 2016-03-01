0459a0bff9cd79753d476d109887bbf004401f95
Patrick Granvold <granvold@gmail.com>
2016-03-01 10:24:01 -0800

    Default security and map record delete integration test fixes

--- integration-tests/src/test/java/org/ihtsdo/otf/mapping/test/rest/SecurityServiceNormalUseTest.java
+++ integration-tests/src/test/java/org/ihtsdo/otf/mapping/test/rest/SecurityServiceNormalUseTest.java
@@ -1,6 +1,27 @@
 package org.ihtsdo.otf.mapping.test.rest;
 
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.FileReader;
+import java.util.Properties;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.apache.log4j.Logger;
+import org.hibernate.envers.AuditReaderFactory;
+import org.hibernate.search.jpa.FullTextEntityManager;
+import org.hibernate.search.jpa.Search;
+import org.ihtsdo.otf.mapping.helpers.MapUserRole;
+import org.ihtsdo.otf.mapping.jpa.MapRecordJpa;
+import org.ihtsdo.otf.mapping.jpa.MapUserJpa;
+import org.ihtsdo.otf.mapping.jpa.services.MappingServiceJpa;
+import org.ihtsdo.otf.mapping.model.MapUser;
 import org.ihtsdo.otf.mapping.rest.SecurityServiceRest;
+import org.ihtsdo.otf.mapping.services.MappingService;
+import org.ihtsdo.otf.mapping.test.other.MapRecordJpaTest;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Assert;
@@ -13,77 +34,135 @@ import org.junit.Test;
  */
 public class SecurityServiceNormalUseTest {
 
-  /** The service. */
-  private SecurityServiceRest service;
+	/** The manager. */
+	private static EntityManager manager;
 
-  /**
-   * Create test fixtures for class.
-   *
-   * @throws Exception the exception
-   */
-  @BeforeClass
-  public static void setupClass() throws Exception {
-    // do nothing
-  }
+	/** The factory. */
+	private static EntityManagerFactory factory;
 
-  /**
-   * Create test fixtures per test.
-   *
-   * @throws Exception the exception
-   */
-  @Before
-  public void setup() throws Exception {
-    service = new SecurityServiceRest();
-  }
+	/** The full text entity manager. */
+	private static FullTextEntityManager fullTextEntityManager;
 
-  /**
-   * Test normal use of the authenticate methods of {@link SecurityServiceRest}.
-   * 
-   * @throws Exception the exception
-   */
-  @Test
-  public void testNormalUseRestSecurity001() throws Exception {
-    try {
-      String authToken = service.authenticate("guest", "guest");
-      Assert.assertEquals(authToken, "guest");
-      authToken = service.authenticate("demo_lead", "demo_lead");
-      Assert.assertEquals(authToken, "demo_lead");
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-  }
+	/** The service. */
+	private static SecurityServiceRest securityService;
+	private static MappingService mappingService;
 
-  /**
-   * Test normal use of logout for {@link SecurityServiceRest}.
-   *
-   * @throws Exception the exception
-   */
-  @Test
-  public void testNormalUseRestSecurity002() throws Exception {
-    service.authenticate("guest", "guest");
-    service.authenticate("demo_lead", "demo_lead");
-    service.logout("guest");
-    service.logout("demo_lead");
-  }
+	/**
+	 * Create test fixtures for class.
+	 *
+	 * @throws Exception
+	 *             the exception
+	 */
+	@BeforeClass
+	public static void setupClass() throws Exception {
 
-  /**
-   * Teardown.
-   *
-   * @throws Exception the exception
-   */
-  @After
-  public void teardown() throws Exception {
-    // do nothing
-  }
+		// create Entity Manager
+		String configFileName = System.getProperty("run.config.test");
+		Logger.getLogger(MapRecordJpaTest.class).info("  run.config.test = " + configFileName);
+		Properties config = new Properties();
+		FileReader in = new FileReader(new File(configFileName));
+		config.load(in);
+		in.close();
+		Logger.getLogger(MapRecordJpaTest.class).info("  properties = " + config);
+		factory = Persistence.createEntityManagerFactory("MappingServiceDS", config);
+		manager = factory.createEntityManager();
 
-  /**
-   * Teardown class.
-   *
-   * @throws Exception the exception
-   */
-  @AfterClass
-  public static void teardownClass() throws Exception {
-    // do nothing
-  }
+
+		securityService = new SecurityServiceRest();
+		mappingService = new MappingServiceJpa();
+
+		// create map users
+		MapUser user = new MapUserJpa();
+		user.setName("guest");
+		user.setUserName("guest");
+		user.setEmail("guest");
+		user.setApplicationRole(MapUserRole.VIEWER);
+		mappingService.addMapUser(user);
+
+		MapUser demoUser = new MapUserJpa();
+		demoUser.setName("demo_lead");
+		demoUser.setUserName("demo_lead");
+		demoUser.setEmail("demo_lead");
+		demoUser.setApplicationRole(MapUserRole.VIEWER);
+		mappingService.addMapUser(demoUser);
+
+	}
+
+	/**
+	 * Create test fixtures per test.
+	 *
+	 * @throws Exception
+	 *             the exception
+	 */
+	@Before
+	public void setup() throws Exception {
+
+	}
+
+	/**
+	 * Test normal use of the authenticate methods of
+	 * {@link SecurityServiceRest}.
+	 * 
+	 * @throws Exception
+	 *             the exception
+	 */
+	@Test
+	public void testNormalUseRestSecurity001() throws Exception {
+		try {
+			String authToken = securityService.authenticate("guest", "guest");
+			Assert.assertEquals(authToken, "guest");
+			authToken = securityService.authenticate("demo_lead", "demo_lead");
+			Assert.assertEquals(authToken, "demo_lead");
+		} catch (Exception e) {
+			fail("Error during login test");
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Test normal use of logout for {@link SecurityServiceRest}.
+	 *
+	 * @throws Exception
+	 *             the exception
+	 */
+	@Test
+	public void testNormalUseRestSecurity002() throws Exception {
+		try {
+			securityService.authenticate("guest", "guest");
+			securityService.authenticate("demo_lead", "demo_lead");
+			securityService.logout("guest");
+			securityService.logout("demo_lead");
+		} catch (Exception e) {
+			fail("Error during logout test");
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Teardown.
+	 *
+	 * @throws Exception
+	 *             the exception
+	 */
+	@After
+	public void teardown() throws Exception {
+		// do nothing
+	}
+
+	/**
+	 * Teardown class.
+	 *
+	 * @throws Exception
+	 *             the exception
+	 */
+	@AfterClass
+	public static void teardownClass() throws Exception {
+		// remove the users
+		for (MapUser user : mappingService.getMapUsers().getMapUsers()) {
+			mappingService.removeMapUser(user.getId());
+		}
+
+		mappingService.close();
+	}
 
 }
