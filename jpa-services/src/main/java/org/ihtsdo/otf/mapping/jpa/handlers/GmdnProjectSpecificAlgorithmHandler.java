556b1c45c8330423e2c59fe7896e0581f7e9cee3
bcarlsenca <bcarlsen@westcoastinformatics.com>
2016-04-01 18:37:37 -0700

    MAP-1244: limit GMDN results if there are too many when querying for name or id.

--- jpa-services/src/main/java/org/ihtsdo/otf/mapping/jpa/handlers/GmdnProjectSpecificAlgorithmHandler.java
+++ jpa-services/src/main/java/org/ihtsdo/otf/mapping/jpa/handlers/GmdnProjectSpecificAlgorithmHandler.java
@@ -1,11 +1,14 @@
 package org.ihtsdo.otf.mapping.jpa.handlers;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.ihtsdo.otf.mapping.jpa.services.ContentServiceJpa;
 import org.ihtsdo.otf.mapping.jpa.services.MetadataServiceJpa;
 import org.ihtsdo.otf.mapping.rf2.Concept;
 import org.ihtsdo.otf.mapping.rf2.Description;
+import org.ihtsdo.otf.mapping.rf2.TreePosition;
 import org.ihtsdo.otf.mapping.services.ContentService;
 import org.ihtsdo.otf.mapping.services.MetadataService;
 
@@ -78,4 +81,56 @@ public class GmdnProjectSpecificAlgorithmHandler extends
       }
     }
   }
+
+  /* see superclass */
+  @Override
+  public List<TreePosition> limitTreePositions(List<TreePosition> treePositions) {
+    // If the tree structure has more than say 100 positions, just return the
+    // top one from each root
+    List<TreePosition> result = new ArrayList<TreePosition>();
+    if (countTreePositions(treePositions) > 100) {
+      for (final TreePosition treePosition : treePositions) {
+        stripTreePosition(treePosition);
+        result.add(treePosition);
+      }
+    } else {
+      return treePositions;
+    }
+
+    return result;
+  }
+
+  /**
+   * Count tree positions.
+   *
+   * @param treePositions the tps
+   * @return the int
+   */
+  private int countTreePositions(List<TreePosition> treePositions) {
+    int i = 0;
+    for (final TreePosition treePosition : treePositions) {
+      i++;
+      i += countTreePositions(treePosition.getChildren());
+    }
+
+    return i;
+
+  }
+
+  /**
+   * Strip tree position.
+   *
+   * @param treePosition the tree position
+   */
+  private void stripTreePosition(TreePosition treePosition) {
+    if (treePosition.getChildren().size() > 1) {
+      final TreePosition firstChild = treePosition.getChildren().get(0);
+      treePosition.getChildren().clear();
+      treePosition.getChildren().add(firstChild);
+    }
+    for (final TreePosition child : treePosition.getChildren()) {
+      stripTreePosition(child);
+    }
+  }
+
 }
