1e0a70e325708b200e4598c445d4158db6064c84
Patrick Granvold <granvold@gmail.com>
2016-04-26 10:24:10 -0700

    Debug content (to be removed)

--- jpa-services/src/main/java/org/ihtsdo/otf/mapping/jpa/handlers/IndexViewerHandler.java
+++ jpa-services/src/main/java/org/ihtsdo/otf/mapping/jpa/handlers/IndexViewerHandler.java
@@ -576,7 +576,7 @@ public class IndexViewerHandler {
 
       // construct the link text and add to details
       for (Fieldable field : d.getFields()) {
-        Logger.getLogger(getClass()).debug(
+        Logger.getLogger(getClass()).info(
             "  " + field.name() + ": " + d.get(field.name()));
       }
 
@@ -597,20 +597,23 @@ public class IndexViewerHandler {
     	htmlFragment += "&nbsp;" + d.get("nemod");
       }
 
+      // add see/see also
+      if (d.get("see") != null) {
+    	  Logger.getLogger(getClass()).info("see found: " + d.get("seeo"));
+    	  htmlFragment += "&nbsp;&mdash;&nbsp;<i>see&nbsp;" + d.get("see") + "</i>";
+      }
+      
+      // add see/see also
+      if (d.get("seealso") != null) {
+    	  Logger.getLogger(getClass()).info("see also found: " + d.get("seealso"));
+    	  htmlFragment += "&nbsp;&mdash;&nbsp;<i>see&nbsp;also&nbsp;" + d.get("seealso") + "</i>";
+      }
+      
       // if code present, add
       if (d.get("code") != null) {
         htmlFragment += "&nbsp;" + d.get("code");
       }
       
-      // add see/see also
-      if (d.get("see") != null) {
-    	  htmlFragment += "&nbsp;&mdash;&nbsp;<i>see&nbsp;" + d.get("see") + "<i>";
-      }
-      
-      // add see/see also
-      if (d.get("seealso") != null) {
-    	  htmlFragment += "&nbsp;&mdash;&nbsp;<i>see&nbsp;also&nbsp;" + d.get("seealso") + "<i>";
-      }
 
       // add line break
       htmlFragment += "<br>";
