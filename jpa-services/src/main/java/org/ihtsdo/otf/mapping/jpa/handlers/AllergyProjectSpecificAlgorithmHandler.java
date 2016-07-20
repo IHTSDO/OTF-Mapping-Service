235b541a696c4ab9a22c862310d9624168c9ea44
bcarlsenca <bcarlsen@westcoastinformatics.com>
2016-07-20 15:30:42 -0700

    DEMO: more changes for demo, including cleaning up a sample medication project.

--- jpa-services/src/main/java/org/ihtsdo/otf/mapping/jpa/handlers/AllergyProjectSpecificAlgorithmHandler.java
+++ jpa-services/src/main/java/org/ihtsdo/otf/mapping/jpa/handlers/AllergyProjectSpecificAlgorithmHandler.java
@@ -1,5 +1,11 @@
 package org.ihtsdo.otf.mapping.jpa.handlers;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.ihtsdo.otf.mapping.helpers.ValidationResult;
+import org.ihtsdo.otf.mapping.helpers.ValidationResultJpa;
 import org.ihtsdo.otf.mapping.jpa.services.ContentServiceJpa;
 import org.ihtsdo.otf.mapping.model.MapEntry;
 import org.ihtsdo.otf.mapping.model.MapRecord;
@@ -48,4 +54,32 @@ DefaultProjectSpecificAlgorithmHandler {
     return mapEntry.getMapRelation();
   }
 
+  /* see superclass */
+  @Override
+  public ValidationResult validateSemanticChecks(MapRecord mapRecord)
+    throws Exception {
+    final ValidationResult result = new ValidationResultJpa();
+
+    // Map record name must share at least one word with target name
+    final Set<String> recordWords =
+        new HashSet<>(Arrays.asList(mapRecord.getConceptName().toLowerCase()
+            .split(" ")));
+    final Set<String> entryWords = new HashSet<>();
+    for (final MapEntry entry : mapRecord.getMapEntries()) {
+      if (entry.getTargetName() != null) {
+        entryWords.addAll(Arrays.asList(entry.getTargetName().toLowerCase()
+            .split(" ")));
+      }
+    }
+    final Set<String> recordMinusEntry = new HashSet<>(recordWords);
+    recordMinusEntry.remove(entryWords);
+
+    // If there are entry words and none match, warning
+    if (entryWords.size() > 0 && recordWords.size() == recordMinusEntry.size()) {
+      result
+          .addWarning("From concept and target code names must share at least one word.");
+    }
+
+    return result;
+  }
 }
