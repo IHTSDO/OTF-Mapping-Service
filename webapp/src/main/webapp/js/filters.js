71aa00c4b9a0e0836f5b1a145ba066ee06ebb75c
Patrick Granvold <granvold@gmail.com>
2016-12-19 13:36:04 -0800

    Initial work on ui-tree directive implementation of terminologyBrowser

--- webapp/src/main/webapp/js/filters.js
+++ webapp/src/main/webapp/js/filters.js
@@ -2,3 +2,38 @@
 
 /* Filters */
 
+mapProjectApp.filter('highlight', function($sce) {
+  return function(text, phrase) {
+    // console.debug("higlight", text, phrase);
+    var htext = text;
+    if (text && phrase) {
+      htext = text.replace(new RegExp('(' + phrase.replace(/"/g, '') + ')', 'gi'),
+        '<span class="highlighted">$1</span>');
+    }
+    return $sce.trustAsHtml(htext);
+  };
+});
+
+mapProjectApp.filter('highlightLabelFor', function($sce) {
+  return function(text, phrase) {
+    // console.debug("higlightLabelFor", text, phrase);
+    var htext = text;
+    if (text && phrase) {
+      htext = text.replace(new RegExp('(' + phrase.replace(/"/g, '') + ')', 'gi'),
+        '<span style="background-color:#e0ffe0;">$1</span>');
+    }
+    return $sce.trustAsHtml(htext);
+  };
+});
+
+mapProjectApp.filter('highlightLabel', function($sce) {
+  return function(text, phrase) {
+    // console.debug("higlightLabel", text, phrase);
+    var htext = text;
+    if (text && phrase) {
+      htext = text.replace(new RegExp('(' + phrase.replace(/"/g, '') + ')', 'gi'),
+        '<span style="background-color:#e0e0ff;">$1</span>');
+    }
+    return $sce.trustAsHtml(htext);
+  };
+});
