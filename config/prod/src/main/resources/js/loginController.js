84b5a3a6bcedc99ab7dc30afab5230d21a3dea70
Patrick Granvold <granvold@gmail.com>
2017-03-23 09:33:11 -0700

    MAP-1415: Address 302 redirect response from IMS

--- config/prod/src/main/resources/js/loginController.js
+++ config/prod/src/main/resources/js/loginController.js
@@ -377,21 +377,30 @@ mapProjectAppControllers.controller('LoginCtrl', [
       // / ims.ihtsdotools.org
       $rootScope.glassPane++;
       $http.get('ims-api/account').then(
-      // / / Success
-      function(response) {
-        // / / Call "go" function
-        $scope.userName = response.data.login;
-        $scope.password = JSON.stringify(response.data);
-        $rootScope.glassPane--;
-        $scope.go();
-      },
-      // / / Error
-      function(response) {
-        // / / $rootScope.globalError = response;
-        // / / Show login buttons
-        $scope.pending = false;
-        $rootScope.glassPane--;
-      });
+        // / / Success
+        function(response) {
+          if (response.status == '302' || response.status == 302) {
+            
+            //https://ims.ihtsdotools.org/#/login?serviceReferer=https:%2F%2Fauthoring.ihtsdotools.org%2F#%2Fhome
+            var referer = $location.protocol() + '://' + $location.host();
+            var redirectUrl = response.headers['Location'] + '?serviceReferer='
+              + encodeURIComponent(referer);
+            $location.path(redirectUrl);
+          } else {
+            // / / Call "go" function
+            $scope.userName = response.data.login;
+            $scope.password = JSON.stringify(response.data);
+            $rootScope.glassPane--;
+            $scope.go();
+          }
+        },
+        // / / Error
+        function(response) {
+          // / / $rootScope.globalError = response;
+          // / / Show login buttons
+          $scope.pending = false;
+          $rootScope.glassPane--;
+        });
 
     }
 
