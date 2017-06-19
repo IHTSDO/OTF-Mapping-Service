5b9d5ca2c9df1aaf4020f4cc2a3e80ada788fb2b
shapirod2 <dshapiro@westcoastinformatics.com>
2017-06-19 10:56:49 -0700

    Add uniq function to utilService.  Sort unique the parents and children lists on the record concept page.

--- rest/src/main/webapp/js/widgets/recordConcept/recordConcept.js
+++ rest/src/main/webapp/js/widgets/recordConcept/recordConcept.js
@@ -14,7 +14,7 @@ angular
   .controller(
     'recordConceptCtrl',
     function($scope, $rootScope, $http, $routeParams, $location, $uibModal, localStorageService, $sce,
-      appConfig) {
+      appConfig, utilService) {
 
       // scope variables
       $scope.appConfig = appConfig;
@@ -88,6 +88,7 @@ angular
           }).success(
           function(data) {
             $scope.concept = data;
+            $scope.concept.relationship = utilService.uniq($scope.concept.relationship, 'destinationConceptPreferredName');
             setTitle($scope.focusProject.sourceTerminology, $routeParams.conceptId,
               $scope.concept.defaultPreferredName);
             $scope.getRecordsForConcept();
@@ -106,7 +107,7 @@ angular
                 }
               }).success(function(data) {
               console.debug(data);
-              $scope.concept.children = data.searchResult;
+              $scope.concept.children = utilService.uniq(data.searchResult, 'value');
 
             }).error(function(data, status, headers, config) {
               $rootScope.handleHttpError(data, status, headers, config);
@@ -130,7 +131,9 @@ angular
       $scope.to_trusted = function(html_code) {
         return $sce.trustAsHtml(html_code);
       };
+      
 
+    	  
       $scope.getRecordsForConcept = function() {
         // retrieve all records with this concept id
         $http({
@@ -272,6 +275,7 @@ angular
         }
       };
 
+        
       $scope.isEditable = function(record) {
 
         if (($scope.currentRole === 'Specialist' || $scope.currentRole === 'Lead' || $scope.currentRole === 'Administrator')
