b57c41d6c4e2a08642f5aa175ae3a928775e8e76
bcarlsenca <bcarlsen@westcoastinformatics.com>
2017-07-30 15:10:58 -0700

    ICD10: Support ability to manage workflow for this project, including: * adding notes on record assignment * removing blocks of records instead of the entire records list * auto creation of QA records

--- admin/mojo/src/main/java/org/ihtsdo/otf/mapping/mojo/AdHocMojo.java
+++ admin/mojo/src/main/java/org/ihtsdo/otf/mapping/mojo/AdHocMojo.java
@@ -1,26 +1,31 @@
+/*
+ *    Copyright 2015 West Coast Informatics, LLC
+ */
 package org.ihtsdo.otf.mapping.mojo;
 
-import java.util.Date;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
-import org.apache.log4j.Logger;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.ihtsdo.otf.mapping.jpa.services.ReportServiceJpa;
+import org.ihtsdo.otf.mapping.helpers.MapRecordList;
+import org.ihtsdo.otf.mapping.helpers.WorkflowAction;
+import org.ihtsdo.otf.mapping.jpa.services.ContentServiceJpa;
+import org.ihtsdo.otf.mapping.jpa.services.MappingServiceJpa;
 import org.ihtsdo.otf.mapping.jpa.services.WorkflowServiceJpa;
-import org.ihtsdo.otf.mapping.model.Feedback;
-import org.ihtsdo.otf.mapping.model.FeedbackConversation;
-import org.ihtsdo.otf.mapping.reports.Report;
+import org.ihtsdo.otf.mapping.model.MapProject;
+import org.ihtsdo.otf.mapping.model.MapRecord;
+import org.ihtsdo.otf.mapping.model.MapUser;
 import org.ihtsdo.otf.mapping.rf2.Concept;
-import org.ihtsdo.otf.mapping.rf2.SimpleRefSetMember;
 import org.ihtsdo.otf.mapping.services.ContentService;
-import org.ihtsdo.otf.mapping.services.ReportService;
+import org.ihtsdo.otf.mapping.services.MappingService;
 import org.ihtsdo.otf.mapping.services.WorkflowService;
-
-import com.google.common.util.concurrent.Service;
 
 /**
  * Customizable mojo to run ad hoc code
@@ -31,297 +36,153 @@ import com.google.common.util.concurrent.Service;
  */
 public class AdHocMojo extends AbstractMojo {
 
-  /** The content service. */
-  ContentService contentService = null;
+  /**
+   * The specified refsetId
+   * @parameter
+   * @required
+   */
+  private String refsetId = null;
 
-  /** The workflow service. */
-  WorkflowService workflowService = null;
+  /**
+   * The specified mode
+   * @parameter
+   * @required
+   */
+  private String mode = null;
+
+  /**
+   * The specified input file (for driving action)
+   * @parameter
+   */
+  private String inputFile = null;
 
   /* see superclass */
   @Override
   public void execute() throws MojoExecutionException, MojoFailureException {
     getLog().info("Start Ad hoc mojo");
+    getLog().info("  refsetId = " + refsetId);
+    getLog().info("  mode = " + mode);
 
-    try {
-      workflowService = new WorkflowServiceJpa();
-      // For UAT
-      Long[] ids = {
-              252L, 253L
-          };
-      for (Long id : ids) {
-    	  FeedbackConversation conversation = workflowService.getFeedbackConversation(id);
-    	  Set<Date> timestamps = new HashSet<>();
-    	  Set<Feedback> feedbackToRemove = new HashSet<>();
-    	  List<Feedback> feedbacks = conversation.getFeedbacks();
-    	  for (Feedback f : feedbacks) {
-    		  if (!timestamps.contains(f.getTimestamp())) {
-    			  timestamps.add(f.getTimestamp());
-    		  } else {
-    			  feedbackToRemove.add(f);
-    		  }
-    	  }
-    	  for (Feedback f : feedbackToRemove) {
-    	    feedbacks.remove(f);
-    	    conversation.setFeedbacks(feedbacks);
-    	    workflowService.updateFeedbackConversation(conversation);
-    	  }
+    try (final WorkflowService workflowService = new WorkflowServiceJpa();
+        final ContentService contentService = new ContentServiceJpa();
+        final MappingService mappingService = new MappingServiceJpa()) {
+
+      if (mode != null && mode.equals("icd11")) {
+        workflowService.setTransactionPerOperation(false);
+        workflowService.beginTransaction();
+        mappingService.setTransactionPerOperation(false);
+        mappingService.beginTransaction();
+        contentService.setTransactionPerOperation(false);
+        contentService.beginTransaction();
+        handleIcd11(refsetId, inputFile, workflowService, contentService,
+            mappingService);
       }
-    } catch (Exception e) {
-        e.printStackTrace();
-        throw new MojoExecutionException("Ad-hoc mojo failed to complete", e);
-    }  finally {
-        try {
-            workflowService.close();
-          } catch (Exception e) {
-            e.printStackTrace();
-            throw new MojoExecutionException(
-                "Ad-hoc mojo failed to close services.", e);
-          }
-        }
-    
-    /*try {
-      ReportService reportService = new ReportServiceJpa();
-
-      Long[] ids = {
-          8796L, 8799L, 8797L, 8798L, 8800L, 8801L, 8802L
-      };
-
-      for (Long id : ids) {
-        try {
-          Report report = reportService.getReport(id);
-          Logger.getLogger(getClass()).info(
-              "Removing report " + report.getId() + ", " + report.getName());
-          if (report != null) {
-            reportService.removeReport(report.getId());
-          }
-        } catch (Exception e) {
-          // do nothing
-        }
-
-      }
-
-    } catch (Exception e1) {
-      // TODO Auto-generated catch block
-      e1.printStackTrace();
-    }*/
-    /**
-     * Put ad hoc code below:
-     * 
-     * 20160226:
-     * 
-     * <pre>
-     * Find all map records from project with refsetid 447562003
-     * If there is a single map entry and it is an asterisk code
-     *   Remove advice “THIS CODE IS NOT TO BE USED IN THE PRIMARY POSITION”
-     *     If it exists
-     *   Add  advice “THIS CODE MAY BE USED IN THE PRIMARY POSITION WHEN THE MANIFESTATION IS THE PRIMARY FOCUS OF CARE”
-     *     If it does not exist
-     *   Add advice “THIS MAP REQUIRES A DAGGER CODE AS WELL AS AN ASTERISK CODE”
-     *     If it does not exist
-     * If there are more than one map ent)ries, for each entry whose targetId is an asterisk code:
-     *   Remove advice “THIS CODE IS NOT TO BE USED IN THE PRIMARY POSITION”
-     * If it exists
-     *   Add  advice “THIS CODE MAY BE USED IN THE PRIMARY POSITION WHEN THE MANIFESTATION IS THE PRIMARY FOCUS OF CARE”
-     *     If it does not exist
-     * </pre>
-     */
-
-    try {
-      /*
-       * contentService = new ContentServiceJpa(); workflowService = new
-       * WorkflowServiceJpa();
-       * 
-       * // want a single commit
-       * workflowService.setTransactionPerOperation(false);
-       * 
-       * // find ICD10 project MapProject mapProject = null; for (MapProject p :
-       * workflowService.getMapProjects().getMapProjects()) { if
-       * (p.getRefSetId().equals("447562003")) { mapProject = p; } }
-       * 
-       * if (mapProject == null) { throw new MojoExecutionException(
-       * "Could not find map project for refset id 447562003"); }
-       * Logger.getLogger(this.getClass()).info( "Found map project " +
-       * mapProject.getName());
-       * 
-       * MapAdvice advice1 = null; MapAdvice advice2 = null; MapAdvice advice3 =
-       * null;
-       * 
-       * // find the desired advice for (MapAdvice m :
-       * workflowService.getMapAdvices().getMapAdvices()) { if
-       * (m.getName().equals( "THIS CODE MAY BE USED IN THE PRIMARY POSITION " +
-       * "WHEN THE MANIFESTATION IS THE PRIMARY FOCUS OF CARE")) { advice1 = m;
-       * }
-       * 
-       * else if (m.getName().equals(
-       * "THIS CODE IS NOT TO BE USED IN THE PRIMARY POSITION")) { advice2 = m;
-       * }
-       * 
-       * else if (m.getName().equals(
-       * "THIS MAP REQUIRES A DAGGER CODE AS WELL AS AN ASTERISK CODE")) {
-       * advice3 = m; } }
-       * 
-       * if (advice1 == null || advice2 == null || advice3 == null) { throw new
-       * MojoExecutionException("Could not find map advices"); }
-       * getLog().info("  Found map advice - " + advice1.getName());
-       * getLog().info("  Found map advice - " + advice2.getName());
-       * getLog().info("  Found map advice - " + advice3.getName());
-       * 
-       * // find the ICD10 asterisk code String asteriskConceptId = null;
-       * 
-       * PfsParameter pfs = new PfsParameterJpa(); pfs.setQueryRestriction(
-       * "terminology:ICD10 AND terminologyVersion:2010 AND defaultPreferredName:\"Asterisk refset\""
-       * ); try { SearchResultList searchResults =
-       * contentService.findConceptsForQuery(null, pfs); if
-       * (searchResults.getCount() != 1) { throw new MojoFailureException(
-       * "Found multiple asterisk concepts for ICD10: " +
-       * searchResults.toString()); } asteriskConceptId =
-       * searchResults.getSearchResults().get(0).getTerminologyId(); } catch
-       * (Exception e) { e.printStackTrace(); throw new
-       * MojoFailureException("Could not find asterisk code for ICD10"); }
-       * 
-       * Logger.getLogger(this.getClass()).info(
-       * "Found hibernate id for ICD10 asterisk refset: " + asteriskConceptId);
-       * 
-       * Logger.getLogger(this.getClass()).info(
-       * "Loading published and publication-ready records...");
-       * 
-       * // count variables for log output int nTotal = 0; int nChanged = 0;
-       * 
-       * // cycle over all map records for project List<MapRecord> mapRecords =
-       * workflowService.getMapRecordsForMapProject(mapProject.getId())
-       * .getMapRecords();
-       * 
-       * Logger.getLogger(this.getClass()).info(
-       * "Cycling over published and publication-ready records...");
-       * 
-       * workflowService.beginTransaction();
-       * 
-       * Iterator<MapRecord> iter = mapRecords.iterator();
-       * 
-       * while (iter.hasNext()) { MapRecord mr = iter.next(); boolean
-       * recordChanged = false;
-       * 
-       * // skip non-published records if (!mr.getWorkflowStatus()
-       * .equals(WorkflowStatus.READY_FOR_PUBLICATION) &&
-       * !mr.getWorkflowStatus().equals(WorkflowStatus.PUBLISHED)) { continue; }
-       * 
-       * nTotal++;
-       * 
-       * // If there is a single map entry and it is an asterisk code // Remove
-       * advice “THIS CODE IS NOT TO BE USED IN THE PRIMARY POSITION” // If it
-       * exists // Add advice “THIS CODE MAY BE USED IN THE PRIMARY POSITION
-       * WHEN THE // MANIFESTATION IS THE PRIMARY FOCUS OF CARE” // If it does
-       * not exist // Add advice “THIS MAP REQUIRES A DAGGER CODE AS WELL AS AN
-       * ASTERISK // CODE” // If it does not exist if (mr.getMapEntries().size()
-       * == 1) { final MapEntry entry = mr.getMapEntries().get(0);
-       * 
-       * if (isIcd10AsteriskCode(entry.getTargetId(), asteriskConceptId)) { if
-       * (entry.getMapAdvices().contains(advice2)) {
-       * entry.removeMapAdvice(advice2); recordChanged = true;
-       * getLog().info("    remove " + mr.getConceptId() + " " + advice2); } if
-       * (!entry.getMapAdvices().contains(advice1)) {
-       * entry.addMapAdvice(advice1); recordChanged = true;
-       * getLog().info("    add " + mr.getConceptId() + " " + advice1); } if
-       * (!entry.getMapAdvices().contains(advice3)) {
-       * entry.addMapAdvice(advice3); recordChanged = true;
-       * getLog().info("    add " + mr.getConceptId() + " " + advice3); } if
-       * (recordChanged) { getLog().info("    changed " + mr.getConceptId());
-       * nChanged++; } } }
-       * 
-       * else {
-       * 
-       * // cycle over entries for (final MapEntry entry : mr.getMapEntries()) {
-       * 
-       * // if an icd10 asterisk code if
-       * (isIcd10AsteriskCode(entry.getTargetId(), asteriskConceptId)) {
-       * 
-       * // If there are more than one map entries, for each entry whose //
-       * targetId is an asterisk code: // Remove advice “THIS CODE IS NOT TO BE
-       * USED IN THE PRIMARY // POSITION” // If it exists // Add advice “THIS
-       * CODE MAY BE USED IN THE PRIMARY POSITION WHEN // THE MANIFESTATION IS
-       * THE PRIMARY FOCUS OF CARE” // If it does not exist
-       * 
-       * if (entry.getMapAdvices().contains(advice2)) {
-       * entry.getMapAdvices().remove(advice2); recordChanged = true;
-       * getLog().info("    remove " + mr.getConceptId() + " " + advice2); }
-       * 
-       * if (!entry.getMapAdvices().contains(advice1)) {
-       * entry.getMapAdvices().add(advice1); recordChanged = true;
-       * getLog().info("    add " + mr.getConceptId() + " " + advice2); } } } if
-       * (recordChanged) { getLog().info("    changed " + mr.getConceptId());
-       * nChanged++; } }
-       * 
-       * if (recordChanged == true) { workflowService.updateMapRecord(mr); } }
-       * 
-       * getLog().info("  changed = " + nChanged); getLog().info("  total = " +
-       * nTotal);
-       * 
-       * getLog().info("Committing...");
-       * 
-       * // execute the transaction workflowService.commit();
-       */
-
-      getLog().info("Finished");
 
     } catch (Exception e) {
       e.printStackTrace();
       throw new MojoExecutionException("Ad-hoc mojo failed to complete", e);
-    } finally {
-      try {
-        workflowService.close();
-      } catch (Exception e) {
-        e.printStackTrace();
-        throw new MojoExecutionException(
-            "Ad-hoc mojo failed to close services.", e);
-      }
     }
-
   }
-
-  // because even in an adhoc lightweight mojo I felt the need to cache query
-  // results
-  /** The icd10 asterisk codes. */
-  Set<String> icd10AsteriskCodes = new HashSet<>();
 
   /**
-   * Indicates whether or not icd10 asterisk code is the case.
+   * Handle icd 11.
    *
-   * @param targetId the target id
-   * @param asteriskCode the asterisk code
-   * @return <code>true</code> if so, <code>false</code> otherwise
+   * @param refsetId the refset id
+   * @param workflowService the workflow service
+   * @param contentService the content service
+   * @param mappingService the mapping service
    * @throws Exception the exception
    */
-  @SuppressWarnings("unused")
-  private boolean isIcd10AsteriskCode(String targetId, String asteriskCode)
-    throws Exception {
-
-    // if empty or null, return
-    if (targetId == null || targetId.isEmpty()) {
-      return false;
+  private void handleIcd11(String refsetId, String inputFile,
+    WorkflowService workflowService, ContentService contentService,
+    MappingService mappingService) throws Exception {
+    if (inputFile == null) {
+      throw new Exception("Unexpectedly null input file.");
+    }
+    if (!new File(inputFile).exists()) {
+      throw new Exception("Input file does not exist = " + inputFile);
     }
 
-    // check cache
-    if (icd10AsteriskCodes.contains(targetId)) {
-      return true;
-    }
-
-    // otherwise get the concept
-    Concept c = contentService.getConcept(targetId, "ICD10", "2010");
-
-    if (c == null) {
-      throw new MojoFailureException("Could not retrieve ICD10 concept "
-          + targetId + ", aborting");
-    }
-
-    for (SimpleRefSetMember srs : c.getSimpleRefSetMembers()) {
-      if (srs.getRefSetId().equals(asteriskCode)) {
-        icd10AsteriskCodes.add(targetId);
-        return true;
+    // Concept ids
+    final Set<String> conceptIds = new HashSet<>();
+    try (final BufferedReader in =
+        new BufferedReader(new FileReader(new File(inputFile)))) {
+      String line = null;
+      while ((line = in.readLine()) != null) {
+        String tokens[] = line.split("\t");
+        conceptIds.add(tokens[5]);
       }
     }
 
-    return false;
+    // Load the map project
+    final Map<String, MapProject> mapProjectMap = new HashMap<>();
+    for (MapProject project : mappingService.getMapProjects().getIterable()) {
+      mapProjectMap.put(project.getRefSetId(), project);
+    }
+    final MapProject project = mapProjectMap.get(refsetId);
+    if (project == null) {
+      throw new Exception(
+          "Unable to find map project for refsetId = " + refsetId);
+    }
+    getLog().info("  map project = " + project.getId() + ","
+        + project.getRefSetId() + ", " + project.getName());
+
+    // Iterate through, load concept, load records, make QA record
+    int ct = 0;
+    for (final String conceptId : conceptIds) {
+      final MapRecordList list = mappingService
+          .getMapRecordsForProjectAndConcept(project.getId(), conceptId);
+      if (list.getCount() != 1) {
+        throw new Exception("Unexpected number of records (" + list.getCount()
+            + ") for project " + project.getId() + ", conceptId = "
+            + conceptId);
+      }
+      createQARecord(workflowService, contentService, project,
+          list.getMapRecords().get(0));
+
+      if (++ct % 500 == 0) {
+        getLog().info("    count = " + ct);
+      }
+      if (++ct % 2000 == 0) {
+        mappingService.commit();
+        mappingService.clear();
+        mappingService.beginTransaction();
+        workflowService.commit();
+        workflowService.clear();
+        workflowService.beginTransaction();
+        contentService.commit();
+        contentService.clear();
+        contentService.beginTransaction();
+      }
+    }
 
   }
 
+  /**
+   * Creates the QA record.
+   *
+   * @param workflowService the workflow service
+   * @param contentService the content service
+   * @param mapRecord the map record
+   * @throws Exception the exception
+   */
+  private void createQARecord(WorkflowService workflowService,
+    ContentService contentService, MapProject mapProject, MapRecord mapRecord)
+    throws Exception {
+
+    final Concept concept = contentService.getConcept(mapRecord.getConceptId(),
+        mapProject.getSourceTerminology(),
+        mapProject.getSourceTerminologyVersion());
+
+    // find the qa user
+    MapUser mapUser = null;
+    for (final MapUser user : workflowService.getMapUsers().getMapUsers()) {
+      if (user.getUserName().equals("qa"))
+        mapUser = user;
+    }
+
+    // process the workflow action
+    workflowService.processWorkflowAction(mapProject, concept, mapUser,
+        mapRecord, WorkflowAction.CREATE_QA_RECORD);
+  }
 }
