23537d2196953f3451d99aea919ddf198a398b3d
shapirod2 <dshapiro@westcoastinformatics.com>
2019-11-05 08:19:34 -0800

    MSSP-400: Reorder notes

--- jpa-model/src/main/java/org/ihtsdo/otf/mapping/helpers/TreePositionDescriptionGroupJpa.java
+++ jpa-model/src/main/java/org/ihtsdo/otf/mapping/helpers/TreePositionDescriptionGroupJpa.java
@@ -1,6 +1,8 @@
 package org.ihtsdo.otf.mapping.helpers;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -33,9 +35,32 @@ public class TreePositionDescriptionGroupJpa implements
 
   /* see superclass */
   @Override
-  public List<TreePositionDescription> getTreePositionDescriptions() {
-    return treePositionDescriptions;
-  }
+	public List<TreePositionDescription> getTreePositionDescriptions() {
+		Comparator<TreePositionDescription> comparator = new Comparator<TreePositionDescription>() {
+
+			@Override
+			public int compare(final TreePositionDescription o1, final TreePositionDescription o2) {
+				if (o1.getName().contains(":") && o2.getName().contains(":")) {
+					String o1Prefix = o1.getName().substring(0, o1.getName().indexOf(':'));
+					String o2Prefix = o2.getName().substring(0, o2.getName().indexOf(':'));
+					if (!o1Prefix.equals(o2Prefix)) {
+						return o1.getName().compareToIgnoreCase(o2.getName());
+					} else if (!o1.getName().endsWith(")")) {
+						return -1;
+					} else if (!o2.getName().endsWith(")")) {
+						return 1;
+					} else {
+						return o1.getName().compareToIgnoreCase(o2.getName());
+					}
+				} else {
+					return o1.getName().compareToIgnoreCase(o2.getName());
+				}
+			}
+
+		};
+		Collections.sort(treePositionDescriptions, comparator);
+		return treePositionDescriptions;
+	}
 
   /* see superclass */
   @Override
@@ -49,7 +74,7 @@ public class TreePositionDescriptionGroupJpa implements
   public void addTreePositionDescription(
     TreePositionDescription treePositionDescription) {
     if (this.treePositionDescriptions == null)
-      this.treePositionDescriptions = new ArrayList<>();
+      this.treePositionDescriptions = new ArrayList<>();    
     this.treePositionDescriptions.add(treePositionDescription);
   }
 
