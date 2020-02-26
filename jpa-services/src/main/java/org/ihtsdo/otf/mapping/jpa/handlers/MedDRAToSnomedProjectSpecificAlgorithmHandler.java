a4453d80255e42b10097f922905373bbd4e14555
Rick Wood <rwood@westcoastinformatics.com>
2020-02-26 09:39:40 -0800

    SNOMED to/from MedDRA projects are being changed to Simple projects, so relations are no longer accepted or allowed

--- jpa-services/src/main/java/org/ihtsdo/otf/mapping/jpa/handlers/MedDRAToSnomedProjectSpecificAlgorithmHandler.java
+++ jpa-services/src/main/java/org/ihtsdo/otf/mapping/jpa/handlers/MedDRAToSnomedProjectSpecificAlgorithmHandler.java
@@ -2,8 +2,6 @@ package org.ihtsdo.otf.mapping.jpa.handlers;
 
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import org.ihtsdo.otf.mapping.helpers.ValidationResult;
@@ -18,15 +16,14 @@ import org.ihtsdo.otf.mapping.services.ContentService;
  * Implementation for sample allergy mapping project. Require valid codes to be
  * allergies.
  */
-public class MedDRAToSnomedProjectSpecificAlgorithmHandler extends
-  DefaultProjectSpecificAlgorithmHandler {
+public class MedDRAToSnomedProjectSpecificAlgorithmHandler
+    extends DefaultProjectSpecificAlgorithmHandler {
 
   /* see superclass */
   @Override
   public ValidationResult validateTargetCodes(MapRecord mapRecord)
-      throws Exception {
+    throws Exception {
 
-    // A relation must be selected
     // Maps cannot have multiple groups
     // Maps cannot have multiple entries
     // If either condition is not met, the changes will not be saved
@@ -39,25 +36,29 @@ public class MedDRAToSnomedProjectSpecificAlgorithmHandler extends
         validationResult.addError("A map record may only have one entry.");
       }
 
-      for (final MapEntry mapEntry : mapRecord.getMapEntries()) {
-        if (mapEntry.getMapRelation() == null) {
-          validationResult
-              .addError("A relation indicating the reason must be selected.");
-        }
-      }
+      // RAW 20200226 - MedDRA to Snomed project is being changes to a Simple
+      // project, so relations are no longer accepted or allowed
+      
+      // for (final MapEntry mapEntry : mapRecord.getMapEntries()) {
+      // if (mapEntry.getMapRelation() == null) {
+      // validationResult
+      // .addError("A relation indicating the reason must be selected.");
+      // }
+      // }
     }
     return validationResult;
-  }
-  
-  /* see superclass */
-  @Override
-  public boolean isTargetCodeValid(String terminologyId) throws Exception {
-	  return true;
   }
 
   /* see superclass */
   @Override
-  public MapRelation computeMapRelation(MapRecord mapRecord, MapEntry mapEntry) {
+  public boolean isTargetCodeValid(String terminologyId) throws Exception {
+    return true;
+  }
+
+  /* see superclass */
+  @Override
+  public MapRelation computeMapRelation(MapRecord mapRecord,
+    MapEntry mapEntry) {
 
     // If the map relation is blank, return "exact" (e.g. the default)
     if (mapEntry.getMapRelation() == null) {
@@ -79,24 +80,25 @@ public class MedDRAToSnomedProjectSpecificAlgorithmHandler extends
     final ValidationResult result = new ValidationResultJpa();
 
     // Map record name must share at least one word with target name
-    final Set<String> recordWords =
-        new HashSet<>(Arrays.asList(mapRecord.getConceptName().toLowerCase()
-            .split(" ")));
+    final Set<String> recordWords = new HashSet<>(
+        Arrays.asList(mapRecord.getConceptName().toLowerCase().split(" ")));
     final Set<String> entryWords = new HashSet<>();
     for (final MapEntry entry : mapRecord.getMapEntries()) {
       if (entry.getTargetName() != null) {
-        entryWords.addAll(Arrays.asList(entry.getTargetName().toLowerCase()
-            .split(" ")));
+        entryWords.addAll(
+            Arrays.asList(entry.getTargetName().toLowerCase().split(" ")));
       }
     }
     final Set<String> recordMinusEntry = new HashSet<>(recordWords);
     recordMinusEntry.removeAll(entryWords);
 
     // If there are entry words and none match, warning
-    //if (entryWords.size() > 0 && recordWords.size() == recordMinusEntry.size()) {
-    //  result
-    //      .addWarning("From concept and target code names must share at least one word.");
-    //}
+    // if (entryWords.size() > 0 && recordWords.size() ==
+    // recordMinusEntry.size()) {
+    // result
+    // .addWarning("From concept and target code names must share at least one
+    // word.");
+    // }
 
     return result;
   }
