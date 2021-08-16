ce8eaacced8bdd8e98329590daa0219295d90c34
Peter Van Ausdeln <pvanaus@ucdavis.edu>
2021-08-16 10:52:06 -0700

    implements ATC API downloader and terminology loader

--- jpa-services/src/main/java/org/ihtsdo/otf/mapping/jpa/algo/AtcDownloadAlgorithm.java
+++ jpa-services/src/main/java/org/ihtsdo/otf/mapping/jpa/algo/AtcDownloadAlgorithm.java
@@ -1,15 +1,14 @@
 package org.ihtsdo.otf.mapping.jpa.algo;
 
-import java.io.BufferedOutputStream;
+import java.time.format.DateTimeFormatter;
+import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.FileWriter;
+import java.time.LocalDateTime; 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Vector;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
+import java.util.Set;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
@@ -17,6 +16,7 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status.Family;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import org.ihtsdo.otf.mapping.algo.Algorithm;
 import org.ihtsdo.otf.mapping.helpers.LocalException;
@@ -26,13 +26,6 @@ import org.ihtsdo.otf.mapping.services.helpers.ProgressListener;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.jcraft.jsch.Channel;
-import com.jcraft.jsch.ChannelSftp;
-import com.jcraft.jsch.ChannelSftp.LsEntry;
-import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.JSchException;
-import com.jcraft.jsch.Session;
-import com.jcraft.jsch.SftpException;
 
 public class AtcDownloadAlgorithm extends RootServiceJpa implements Algorithm {
 
@@ -55,7 +48,7 @@ public class AtcDownloadAlgorithm extends RootServiceJpa implements Algorithm {
   @SuppressWarnings("rawtypes")
   @Override
   public void compute() throws Exception {
-    Logger.getLogger(getClass()).info("Checking GMDN SFTP for undownloaded terminologies");
+    Logger.getLogger(getClass()).info("Checking ATC API for recent terminologies");
     
     // Pull ATC codes from API  
     final String url = "https://api.helsedirektoratet.no/legemidler/legemidler/atc";
@@ -63,42 +56,80 @@ public class AtcDownloadAlgorithm extends RootServiceJpa implements Algorithm {
      final Client client = ClientBuilder.newClient();
      final String accept = "*/*";
     
-      int skip = 0;
-    
-      while (true) {
+	 int skip = 0;
+	 final ObjectMapper mapper = new ObjectMapper();
+	 
+	 //housekeeping for creating folder later
+	 DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy_MM_dd");  
+	 LocalDateTime now = LocalDateTime.now();  
+	 String dateFormat = dtf.format(now);
+	 
+	 String parentChild = "";
+	 String conceptDesc = "";
+	 Set<String> conceptsMap = new HashSet<String>();
+	 
+	 File folder = new File(ConfigUtility.getConfigProperties().getProperty("gmdnsftp.atcdir") + "/" + dateFormat);
+	  
+	 while (true) {
+	
+	   String targetUri = url + "?skip="+skip+"&take=1000";
+	   WebTarget target = client.target(targetUri);
+	   target = client.target(targetUri);
+	   Logger.getLogger(getClass()).info(targetUri);
+	
+	   Response response =
+	       target.request(accept)
+	       .header("Ocp-Apim-Subscription-Key","b104d51d1ee54769aa8cab321f2f9f98")
+	       .get();
+	   String resultString = response.readEntity(String.class);
+	   if (response.getStatusInfo().getFamily() == Family.SUCCESSFUL) {
+	     // n/a
+	   } else {
+	     throw new LocalException(
+	         "Unexpected terminology server failure. Message = " + resultString);
+	   }
+	   
+	   final JsonNode doc = mapper.readTree(resultString);
 
-        String targetUri = url + "?skip="+skip+"&take=10";
-        WebTarget target = client.target(targetUri);
-        target = client.target(targetUri);
-        Logger.getLogger(getClass()).info(targetUri);
-
-        Response response =
-            target.request(accept)
-            .header("Ocp-Apim-Subscription-Key","b104d51d1ee54769aa8cab321f2f9f98")
-            .get();
-        String resultString = response.readEntity(String.class);
-        if (response.getStatusInfo().getFamily() == Family.SUCCESSFUL) {
-          // n/a
-        } else {
-          throw new LocalException(
-              "Unexpected terminology server failure. Message = " + resultString);
-        }
-
-        final ObjectMapper mapper = new ObjectMapper();
-        final JsonNode doc = mapper.readTree(resultString);
-        // get total amount
-        // Get concepts returned in this call (up to 100)
-
-        for (final JsonNode conceptNode : doc) {
-          JsonNode atcKode = doc.get("atcKode");
-          if(atcKode != null) {
-             System.out.println("atcKode = " + atcKode.asText());
-          }
-        }
-        
-        skip+=1000;
-        break;
-      } 
+	   // create folder if necessary
+	   if(doc.size() > 0 && !folder.exists()) {
+		   folder.mkdir();
+	   }
+	
+	   // get total amount
+	   // Get concepts returned in this call (up to 1000)
+	   for (final JsonNode conceptNode : doc) {
+	     JsonNode atcKode = conceptNode.get("atcKode");
+	     if(atcKode != null) {
+	        System.out.println("atcKode = " + atcKode.asText());
+	        conceptDesc += atcKode.asText() + "|" + conceptNode.get("nivånavn").asText() + System.lineSeparator();
+	        String parent = atcKode.asText();
+	        while(parent != "") {
+	        	if(conceptsMap.contains(parent))
+	        		break;
+	        	parent = StringUtils.chop(parent);
+	        }
+	        if(parent == "") {
+	        	parentChild += "root|" + atcKode.asText() + System.lineSeparator();
+	        }
+	        else {
+	        	parentChild += parent + "|" + atcKode.asText() + System.lineSeparator();
+	        }
+	        conceptsMap.add(atcKode.asText());
+	     }
+	   }
+	    
+	   skip+=1000;
+	   if(doc.size() < 1000) // <1000 => last one, partial return
+		   break;
+	 }
+	 BufferedWriter writer = new BufferedWriter(new FileWriter(folder + "/parent_child.txt"));
+	 writer.write(parentChild);
+	 writer.close();
+	 
+	 writer = new BufferedWriter(new FileWriter(folder + "/concepts.txt"));
+	 writer.write(conceptDesc);
+	 writer.close();
   }
 
   @Override
