e92caeb52fca99f585f10f3d40920deb085ad407
RichardAWood <rickawood@gmail.com>
2022-05-31 08:49:48 -0700

    Adding map relation calculation to ICD10NO projects.

--- jpa-services/src/main/java/org/ihtsdo/otf/mapping/jpa/handlers/ICD10NOProjectSpecificAlgorithmHandler.java
+++ jpa-services/src/main/java/org/ihtsdo/otf/mapping/jpa/handlers/ICD10NOProjectSpecificAlgorithmHandler.java
@@ -139,6 +139,106 @@ public class ICD10NOProjectSpecificAlgorithmHandler extends DefaultProjectSpecif
   }
 
   /**
+   * Computes the map relation for the SNOMEDCT to ICD10NO map project. Based
+   * solely on whether an entry has a TRUE rule or not. No advices are computed
+   * for this project.
+   *
+   * @param mapRecord the map record
+   * @param mapEntry the map entry
+   * @return the map relation
+   * @throws Exception the exception
+   */
+  @Override
+  public MapRelation computeMapRelation(MapRecord mapRecord, MapEntry mapEntry)
+    throws Exception {
+
+    if (mapEntry == null) {
+      return null;
+    }
+    // if entry has no target
+    if (mapEntry.getTargetId() == null || mapEntry.getTargetId().isEmpty()) {
+
+      // if a relation is already set, and is allowable for null target,
+      // keep it
+      if (mapEntry.getMapRelation() != null
+          && mapEntry.getMapRelation().isAllowableForNullTarget())
+        return mapEntry.getMapRelation();
+      else {
+        // retrieve the not classifiable relation
+        // 447638001 - Map source concept cannot be classified with available
+        // data
+        for (final MapRelation relation : mapProject.getMapRelations()) {
+          if (relation.getTerminologyId().equals("447638001"))
+            return relation;
+        }
+
+        // if cannot find, return null
+        return null;
+      }
+    }
+
+    // if rule is not set, return null
+    if (mapEntry.getRule() == null || mapEntry.getRule().isEmpty()) {
+      return null;
+    }
+
+    // if entry has a gender rule
+    if (mapEntry.getRule().contains("MALE")) {
+
+      // retrieve the relations by terminology id
+      // 447639009 - Map of source concept is context dependent
+      for (final MapRelation relation : mapProject.getMapRelations()) {
+        if (relation.getTerminologyId().equals("447639009")) {
+          return relation;
+        }
+      }
+
+      // if entry has an age rule
+    } else if (mapEntry.getRule().contains("AGE")) {
+
+      // retrieve the relations by terminology id
+      // 447639009 - Map of source concept is context dependent
+      for (final MapRelation relation : mapProject.getMapRelations()) {
+        if (relation.getTerminologyId().equals("447639009")) {
+          return relation;
+        }
+      }
+
+      // if the entry has a non-gender, non-age IFA
+    } else if (mapEntry.getRule().startsWith("IFA")) {
+
+      // retrieve the relations by terminology id
+      // 447639009 - Map of source concept is context dependent
+      for (final MapRelation relation : mapProject.getMapRelations()) {
+        if (relation.getTerminologyId().equals("447639009")) {
+          return relation;
+        }
+      }
+
+      // using contains here to capture TRUE and OTHERWISE TRUE
+    } else if (mapEntry.getRule().contains("TRUE")) {
+
+      // retrieve the relations by terminology id
+      for (final MapRelation relation : mapProject.getMapRelations()) {
+        // 447637006 - Map source concept is properly classified
+        if (relation.getTerminologyId().equals("447637006")) {
+          return relation;
+        }
+      }
+
+      // if entry has a target and not TRUE rule
+    } else {
+
+      throw new Exception("Unexpected map relation condition.");
+    }
+
+    // if relation not found, return null
+    return null;
+
+  }
+  
+  
+  /**
    * Cache existing maps.
    *
    * @throws Exception the exception
