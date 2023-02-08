c149b7190cb68b7271244b64e4c32181c043fc75
RichardAWood <rickawood@gmail.com>
2023-02-08 09:54:54 -0800

    Updating MIMS Project handlers to allow "No target" targets

--- jpa-services/src/main/java/org/ihtsdo/otf/mapping/jpa/handlers/MIMSConditionToSnomedProjectSpecificAlgorithmHandler.java
+++ jpa-services/src/main/java/org/ihtsdo/otf/mapping/jpa/handlers/MIMSConditionToSnomedProjectSpecificAlgorithmHandler.java
@@ -53,6 +53,11 @@ public class MIMSConditionToSnomedProjectSpecificAlgorithmHandler
 
     for (final MapEntry mapEntry : mapRecord.getMapEntries()) {
 
+      // "No target" targets are valid
+      if( mapEntry.getTargetId().isBlank()) {
+        continue;
+      }
+      
       // Target code must be an existing concept
       final Concept concept = contentService.getConcept(mapEntry.getTargetId(),
           mapProject.getDestinationTerminology(), mapProject.getDestinationTerminologyVersion());
@@ -115,6 +120,11 @@ public class MIMSConditionToSnomedProjectSpecificAlgorithmHandler
     validSemanticTagsList.add("(event)");
     validSemanticTagsList.add("(situation)");
 
+    // "No target" targets are valid
+    if(terminologyId.isBlank()) {
+      return true;
+    }
+    
     final ContentService contentService = new ContentServiceJpa();
 
     try {
