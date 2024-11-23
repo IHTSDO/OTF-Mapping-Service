4213674ca76eac2ba39a55018bc2dc6115c5d47b
RichardAWood <rickawood@gmail.com>
2024-11-22 16:31:19 -0800

    WMT-259: Finishing setup for ICD10CA/ICD11 ReleaseToRF2ImportMojo

--- mojo/src/main/java/org/ihtsdo/otf/mapping/mojo/ICD11ToICD10CAReleaseToRF2ImportMojo.java
+++ mojo/src/main/java/org/ihtsdo/otf/mapping/mojo/ICD11ToICD10CAReleaseToRF2ImportMojo.java
@@ -9,14 +9,8 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.maven.plugin.AbstractMojo;
@@ -28,23 +22,22 @@ import org.ihtsdo.otf.mapping.jpa.services.ContentServiceJpa;
 import org.ihtsdo.otf.mapping.jpa.services.MappingServiceJpa;
 import org.ihtsdo.otf.mapping.model.AdditionalMapEntryInfo;
 import org.ihtsdo.otf.mapping.model.MapEntry;
-import org.ihtsdo.otf.mapping.model.MapNote;
 import org.ihtsdo.otf.mapping.model.MapProject;
 import org.ihtsdo.otf.mapping.model.MapRecord;
 import org.ihtsdo.otf.mapping.services.ContentService;
 import org.ihtsdo.otf.mapping.services.MappingService;
 
 /**
- * Create the CIHI ICD10CA to ICD11 RF2 import files from *
+ * Create the CIHI ICD11 to ICD10CA RF2 import files from *
  * der2_iissscRefset_ComplexMapActiveSnapshot_INT_YYYYMMDD.txt
  * 
- * mvn package -PCihiICD10CAToICD11ReleaseToRF2Import "-DreleaseFile=<full path to
+ * mvn package -P CihiICD11ToICD10CAReleaseToRF2Import "-DreleaseFile=<full path to
  * der2_iissscRefset_ComplexMapActiveSnapshot_INT_YYYYMMDD.txt file> -Dcurrent.refset.id=<current, published project refset id>"
  * -Dnew.refset.id=<new project refset id>"
  * 
- * @goal cihi-icd10ca-to-icd11-release-to-rf2-import
+ * @goal cihi-icd11-to-icd10ca-release-to-rf2-import
  */
-public class ICD10CAToICD11ReleaseToRF2ImportMojo extends AbstractMojo {
+public class ICD11ToICD10CAReleaseToRF2ImportMojo extends AbstractMojo {
 
   /**
    * Release file.
@@ -74,14 +67,14 @@ public class ICD10CAToICD11ReleaseToRF2ImportMojo extends AbstractMojo {
    * Map project Id (looked up from currentRefsetId).
    */
   private Long mapProjectId;
-
-  private Map<String, ExternalData> sourceConceptToExternalData = new HashMap<>();
-
+  
+  private Map<String, ExternalData> sourceConceptToExternalData = new HashMap<>();  
+  
   /* see superclass */
   @Override
   public void execute() throws MojoExecutionException, MojoFailureException {
 
-    getLog().info("Start ICD10CA to ICD11 Release To RF2 Import Mojo");
+    getLog().info("Start ICD11 to ICD10CA Release To RF2 Import Mojo");
 
     if (StringUtils.isEmpty(releaseFile)) {
       getLog().error("Parameter releaseFile is empty.");
@@ -98,74 +91,65 @@ public class ICD10CAToICD11ReleaseToRF2ImportMojo extends AbstractMojo {
       runConvert(rf);
 
     } catch (Exception e) {
-      getLog().error("Error running ICD10CA to ICD11 Release To RF2 Import Mojo.", e);
+      getLog().error("Error running ICD11 to ICD10CA Release To RF2 Import Mojo.", e);
     }
   }
 
   //
   // Look up additional data not present in the release file
-  // Most are pulled from database objects, but a few are pulled from external
-  // files
+  // Most are pulled from database objects, but a few are pulled from external files
   //
-  private void getExternalData() throws Exception {
+  private void getExternalData()  throws Exception {
 
     getLog().info("Gathering mapping data stored outside of the release file");
 
-    // Get all map records for the project
-
+    
+    
     MappingService mappingService = new MappingServiceJpa();
     ContentService contentService = new ContentServiceJpa();
     MapProject mapProject = mappingService.getMapProjectForRefSetId(currentRefsetId);
     mapProjectId = mapProject.getId();
-
+    
+    // Get all map records for the project
     MapRecordList allMapRecords = mappingService.getMapRecordsForMapProject(mapProjectId);
 
-    for (MapRecord mapRecord : allMapRecords.getMapRecords()) {
+    for(MapRecord mapRecord : allMapRecords.getMapRecords()) {
       // Only process READY_FOR_PUBLICATION and PUBLISHED records
-      if (!(mapRecord.getWorkflowStatus().equals(WorkflowStatus.READY_FOR_PUBLICATION)
-          || mapRecord.getWorkflowStatus().equals(WorkflowStatus.PUBLISHED))) {
+      if(!(mapRecord.getWorkflowStatus().equals(WorkflowStatus.READY_FOR_PUBLICATION) || mapRecord.getWorkflowStatus().equals(WorkflowStatus.PUBLISHED))) {
         continue;
       }
 
-      // Create an External Data object, based on the source concept
+      //Create an External Data object, based on the source concept
       ExternalData externalData = new ExternalData(mapRecord.getConceptId());
-
-      // Look up map notes
-      Set<MapNote> mapNotes = mapRecord.getMapNotes();
-      if(mapNotes != null) {
-        externalData.setMapNotes(mapNotes);
-      }
+            
       
-      // Look up Additional Entry Info
-      for (MapEntry mapEntry : mapRecord.getMapEntries()) {
-        for (AdditionalMapEntryInfo additionalMapEntryInfo : mapEntry
-            .getAdditionalMapEntryInfos()) {
-          if (additionalMapEntryInfo.getField().equals("Relation - Cluster")) {
-            externalData.setRelationCluster(additionalMapEntryInfo.getValue());
-            externalData.setRelationClusterPriority(mapEntry.getMapPriority());
-          } else if (additionalMapEntryInfo.getField().equals("Relation - Target")) {
+      //Look up Additional Entry Info
+      for(MapEntry mapEntry : mapRecord.getMapEntries()) {
+        for(AdditionalMapEntryInfo additionalMapEntryInfo : mapEntry.getAdditionalMapEntryInfos()) {
+          if(additionalMapEntryInfo.getField().equals("Relation - Target")) {
             externalData.setRelationTarget(additionalMapEntryInfo.getValue());
-          } else if (additionalMapEntryInfo.getField().equals("Unmappable Reason")) {
-            externalData.setUnmappableReason(additionalMapEntryInfo.getValue());
-          } else if (additionalMapEntryInfo.getField().equals("Relation - WHO")) {
-            externalData.setRelationWHO(additionalMapEntryInfo.getValue());
-          } else if (additionalMapEntryInfo.getField().equals("Target Mismatch Reason")) {
-            externalData.setTargetMismatchReason(additionalMapEntryInfo.getValue());
           }
+          else if(additionalMapEntryInfo.getField().equals("Unmappable Reason")) {
+            externalData.setUnmappableReason(additionalMapEntryInfo.getValue());
+          }
+          else if(additionalMapEntryInfo.getField().equals("Target Mismatch Reason")) {
+            externalData.setTargetMismatchReason(additionalMapEntryInfo.getValue());
+          } 
         }
       }
-
-      // Add the External Data object to the map
+      
+      //Add the External Data object to the map
       sourceConceptToExternalData.put(mapRecord.getConceptId(), externalData);
-
+      
     }
-
+    
     mappingService.close();
     contentService.close();
-
+    
   }
-
-  /**
+  
+  
+/**
    * Run convert.
    *
    * @param releaseFile the release file
@@ -176,11 +160,8 @@ public class ICD10CAToICD11ReleaseToRF2ImportMojo extends AbstractMojo {
     try {
 
       // Use the records to create an RF2 file including additional map entry info
-      createRF2WithAdditionalInfo(releaseFile);
-
-      // Now create a Map Notes RF2 file
-      createMapNotesRF2(releaseFile);
-
+      createRF2WithAdditionalInfo(releaseFile);    
+      
     } catch (Exception e) {
       e.printStackTrace();
     }
@@ -210,9 +191,8 @@ public class ICD10CAToICD11ReleaseToRF2ImportMojo extends AbstractMojo {
             // Handle header line, adding additional info to the existing 
             if(columns[0].equals("id")) {
               newLine.append(String.join("\t", columns));
-              newLine.append("\t").append("Relation - Target").append("\t").append("Relation - Cluster")              
-              .append("\t").append("Unmappable Reason").append("\t").append("Target Mismatch Reason")
-              .append("\t").append("Relation - WHO").append("\t").append("END");
+              newLine.append("\t").append("Relation - Target").append("\t").append("Unmappable Reason")
+              .append("\t").append("Target Mismatch Reason").append("\t").append("END");
             }
             //Handle data rows
             else {
@@ -243,37 +223,24 @@ public class ICD10CAToICD11ReleaseToRF2ImportMojo extends AbstractMojo {
               final String group = columns[6];
               final String priority = columns[7];
                             
-              //Group 1 Priority 1 gets the relation target, if there is a target
+              //Group 1 Priority 1 gets the relation target if there is a target,
+              // and relation target and unmappable reason if there is no target
               if(group.equals("1") && priority.equals("1") && !columns[10].contentEquals("")) {
-                newLine.append("\t").append(externalData.getRelationTarget());
+                newLine.append("\t").append(externalData.getRelationTarget()).append("\t").append("");
               }
-              else {
-                newLine.append("\t").append("");
-              }
-              
-              //Group 1, final priority gets the relation cluster, if there is a target
-              if(group.equals("1") && externalData.getRelationClusterPriority() != null && priority.equals(externalData.getRelationClusterPriority().toString()) && columns[5] != null) {
-                newLine.append("\t").append(externalData.getRelationCluster());
-              }
-              else {
-                newLine.append("\t").append("");
-              }
-
-              //Group 1, Priority 1 gets the unmappable reason, if there is no target
-              if(group.equals("1") && priority.equals("1") && columns[10].contentEquals("")) {
-                newLine.append("\t").append(externalData.getUnmappableReason());
-              }
-              else {
-                newLine.append("\t").append("");
-              }              
-              
-              //Group 2, Priority 1 get Target Mismatch Reason and Relation - WHO
-              if(group.equals("2") && priority.equals("1")) {
-                newLine.append("\t").append(externalData.getTargetMismatchReason())
-                    .append("\t").append(externalData.getRelationWHO());
+              else if (group.equals("1") && priority.equals("1") && columns[10].contentEquals("")){
+                newLine.append("\t").append(externalData.getRelationTarget()).append("\t").append(externalData.getUnmappableReason());                
               }
               else {
                 newLine.append("\t").append("").append("\t").append("");
+              }     
+              
+              //Group 2, Priority 1 get Target Mismatch Reason
+              if(group.equals("2") && priority.equals("1")) {
+                newLine.append("\t").append(externalData.getTargetMismatchReason());
+              }
+              else {
+                newLine.append("\t").append("");
               }     
               
               //Finally, add "END" (this is used by the Additional Map Entry Info loader)
@@ -291,53 +258,7 @@ public class ICD10CAToICD11ReleaseToRF2ImportMojo extends AbstractMojo {
     } catch (IOException e) {
         System.err.println("Error processing the file: " + e.getMessage());
     }
-  }
-
-  private void createMapNotesRF2(final File releaseFile)
-    throws Exception {
-
-    
-    String outputFilePath = releaseFile.getParent() + "/" + releaseFile.getName().replace(".txt", "_MapNotes_For" + newRefsetId + "Import.txt");   
-    
-    
-    try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFilePath))) {
-
-      StringBuilder newLine = new StringBuilder();
-      
-      // Write the header line
-      newLine.append("id").append("\t").append("effectiveTime").append("\t").append("active").append("\t").append("moduleId").append("\t")
-      .append("refsetId").append("\t").append("referencedComponentId").append("\t").append("referencedComponentName").append("\t").append("note");
-        
-      writer.write(newLine.toString());
-      writer.newLine();
-            
-      // Sort the source concept ids
-      List<String> sortedSourceConceptIds = new ArrayList<>(sourceConceptToExternalData.keySet());
-      Collections.sort(sortedSourceConceptIds);
-      
-      SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd");
-      
-      // For every source concept id, add a line for every map note
-      for(String sourceConceptId : sortedSourceConceptIds) {
-        ExternalData externalData = sourceConceptToExternalData.get(sourceConceptId);
-        for(MapNote mapNote : externalData.getMapNotes()) {
-          newLine = new StringBuilder();
-          newLine.append("").append("\t").append(formatter.format(mapNote.getTimestamp())).append("\t").append("1").append("\t").append("2").append("\t")
-          .append(newRefsetId).append("\t").append(sourceConceptId).append("\t").append("").append("\t").append(mapNote.getNote());
-          
-          // Write the modified line to the output file
-          writer.write(newLine.toString());
-          writer.newLine();
-        }
-      }
-      
-      System.out.println("Conversion completed. Updated data written to " + outputFilePath);
-  
-    } catch (IOException e) {
-        System.err.println("Error processing the file: " + e.getMessage());
-    }
-
-  }
+  }  
 
   /**
    * The External Data.
@@ -347,20 +268,12 @@ public class ICD10CAToICD11ReleaseToRF2ImportMojo extends AbstractMojo {
 
     /** The source code. */
     private String sourceCode;
-
-    private String relationTarget;
-
-    private String relationCluster;
-
-    private Integer relationClusterPriority;
     
+    private String relationTarget;
+        
     private String unmappableReason;
 
-    private String relationWHO;
-
     private String targetMismatchReason;
-    
-    private Set<MapNote> mapNotes = new HashSet<>();
 
     /**
      * Instantiates a {@link ExternalData} from the specified parameters.
@@ -388,38 +301,12 @@ public class ICD10CAToICD11ReleaseToRF2ImportMojo extends AbstractMojo {
       this.relationTarget = relationTarget;
     }
 
-    public String getRelationCluster() {
-      return relationCluster;
-    }
-
-    public void setRelationCluster(String relationCluster) {
-      this.relationCluster = relationCluster;
-    }
-
-    public Integer getRelationClusterPriority() {
-      return relationClusterPriority;
-    }
-
-    public void setRelationClusterPriority(Integer relationClusterPriority) {
-      this.relationClusterPriority = relationClusterPriority;
-    }
-
     public String getUnmappableReason() {
       return unmappableReason;
     }
 
     public void setUnmappableReason(String unmappableReason) {
       this.unmappableReason = unmappableReason;
-    }
-
-
-
-    public String getRelationWHO() {
-      return relationWHO;
-    }
-
-    public void setRelationWHO(String relationWHO) {
-      this.relationWHO = relationWHO;
     }
 
     public String getTargetMismatchReason() {
@@ -429,13 +316,6 @@ public class ICD10CAToICD11ReleaseToRF2ImportMojo extends AbstractMojo {
     public void setTargetMismatchReason(String targetMismatchReason) {
       this.targetMismatchReason = targetMismatchReason;
     }
-
-    public Set<MapNote> getMapNotes() {
-      return mapNotes;
-    }
-
-    public void setMapNotes(Set<MapNote> mapNotes) {
-      this.mapNotes = mapNotes;
-    }    
   }
+
 }
